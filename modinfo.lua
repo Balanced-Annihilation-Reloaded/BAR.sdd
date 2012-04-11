ad004287995e2a95fdf283f21ca44b1a82681e95
beherith <109391+Beherith@users.noreply.github.com>
2012-04-11 21:28:51 +0000

    Added BAR to branch BAR

---
+++ modinfo.lua
@@ -0,0 +1,10 @@
+return {
+  name='Balanced Annihilation',
+  description='Moooooo!',
+  shortname='BA',
+  version='V7.63 Remake v2',
+  mutator='Official',
+  game='Total Annihilation',
+  shortGame='TA',
+  modtype=1,
+}
---
+++ weapons/commander_blast.lua
@@ -0,0 +1,21 @@
+return {
+	commander_blast = {
+		areaofeffect = 720,
+		craterboost = 6,
+		cratermult = 3,
+		edgeeffectiveness = 0.25,
+		explosiongenerator = "custom:FLASHNUKE1280",
+		impulseboost = 3,
+		impulsefactor = 3,
+		name = "Matter/AntimatterExplosion",
+		range = 380,
+		reloadtime = 3.5999999046326,
+		soundhit = "newboom",
+		soundstart = "largegun",
+		turret = 1,
+		weaponvelocity = 250,
+		damage = {
+			default = 50000,
+		},
+	},
+}

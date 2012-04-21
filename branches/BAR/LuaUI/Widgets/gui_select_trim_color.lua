cc6e1ba3a3f25326669ad35d31fe221229cb4e62
VeryBadSoldier <VeryBadSoldier@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2012-04-21 20:22:21 +0000

    BAR: converted tabs to whitespaces

--- branches/BAR/LuaUI/Widgets/gui_select_trim_color.lua
+++ branches/BAR/LuaUI/Widgets/gui_select_trim_color.lua
@@ -1,14 +1,14 @@
 
 function widget:GetInfo()
-	return {
-		name      = 'Select Trim Color',
-		desc      = 'Adds GUI to select trim color',
-		author    = 'very_bad_soldier',
-		date      = 'April 2012',
-		license   = 'GNU GPL v2',
-		layer     = -100,
-		enabled   = true,
-	}
+    return {
+        name      = 'Select Trim Color',
+        desc      = 'Adds GUI to select trim color',
+        author    = 'very_bad_soldier',
+        date      = 'April 2012',
+        license   = 'GNU GPL v2',
+        layer     = -100,
+        enabled   = true,
+    }
 end
 
 --------------------------------------------------------------------------------
@@ -99,89 +99,89 @@ function HSVtoRGB(h, s, v)
 end
 
 function SetNewHue(hue)
-	curHsvColor.h = hue
-	UpdateRgb()
+    curHsvColor.h = hue
+    UpdateRgb()
 end
 
 function SetNewSatAndVal(sat, val)
-	curHsvColor.s = sat
-	curHsvColor.v = val
-	UpdateRgb()
+    curHsvColor.s = sat
+    curHsvColor.v = val
+    UpdateRgb()
 end
 
 function UpdateRgb()
-	curRgbColor = HSVtoRGB( curHsvColor.h, curHsvColor.s, curHsvColor.v )
+    curRgbColor = HSVtoRGB( curHsvColor.h, curHsvColor.s, curHsvColor.v )
 end
 
 function DrawColorPicker()
-	local markerX = colorBarWidthOverall * curHsvColor.h
-	-- Text
-	glPushMatrix()
-	glTranslate( defaultMargin, -defaultMargin, 0.0 )
+    local markerX = colorBarWidthOverall * curHsvColor.h
+    -- Text
+    glPushMatrix()
+    glTranslate( defaultMargin, -defaultMargin, 0.0 )
     glBeginText()
     glText( captionText, 0, 0, fontSize, 'a')
-	glEndText()
-	glPopMatrix()
-	
-	--color bar
-	glPushMatrix()
-	glTranslate( defaultMargin, -colorBarY, 0.0 )
-	glShape(GL.TRIANGLE_STRIP, {
-      { v = { 0, -colorBarHeight }, 					c = { 1.0, 0.0, 0.0, colorBarAlpha } },
-      { v = { 0, 0 }, 									c = { 1.0, 0.0, 0.0, colorBarAlpha } },
-	  { v = { colorBarSegWidth, -colorBarHeight }, 		c = { 0.0, 1.0, 0.0, colorBarAlpha } },
-	  { v = { colorBarSegWidth, 0 }, 					c = { 0.0, 1.0, 0.0, colorBarAlpha } },
-	  { v = { 2 * colorBarSegWidth, -colorBarHeight }, 	c = { 0.0, 0.0, 1.0, colorBarAlpha } },
-	  { v = { 2 * colorBarSegWidth, 0 }, 				c = { 0.0, 0.0, 1.0, colorBarAlpha } },
-	  { v = { 3 * colorBarSegWidth, -colorBarHeight }, 	c = { 1.0, 0.0, 0.0, colorBarAlpha } },
-	  { v = { 3 * colorBarSegWidth, 0 }, 				c = { 1.0, 0.0, 0.0, colorBarAlpha } },
+    glEndText()
+    glPopMatrix()
+    
+    --color bar
+    glPushMatrix()
+    glTranslate( defaultMargin, -colorBarY, 0.0 )
+    glShape(GL.TRIANGLE_STRIP, {
+      { v = { 0, -colorBarHeight },                     c = { 1.0, 0.0, 0.0, colorBarAlpha } },
+      { v = { 0, 0 },                                   c = { 1.0, 0.0, 0.0, colorBarAlpha } },
+      { v = { colorBarSegWidth, -colorBarHeight },      c = { 0.0, 1.0, 0.0, colorBarAlpha } },
+      { v = { colorBarSegWidth, 0 },                    c = { 0.0, 1.0, 0.0, colorBarAlpha } },
+      { v = { 2 * colorBarSegWidth, -colorBarHeight },  c = { 0.0, 0.0, 1.0, colorBarAlpha } },
+      { v = { 2 * colorBarSegWidth, 0 },                c = { 0.0, 0.0, 1.0, colorBarAlpha } },
+      { v = { 3 * colorBarSegWidth, -colorBarHeight },  c = { 1.0, 0.0, 0.0, colorBarAlpha } },
+      { v = { 3 * colorBarSegWidth, 0 },                c = { 1.0, 0.0, 0.0, colorBarAlpha } },
     })
-	glPopMatrix()
-	
-	--selected color box
-	glPushMatrix()
-	glTranslate( colorBarWidthOverall + 2*defaultMargin, -selectedColorBoxY, 0.0 )
-	glColor( curRgbColor.r, curRgbColor.g, curRgbColor.b, 1.0 )
-	glRect( 0.0, 0.0, selectedColorRectWidth, -selectedColorRectWidth )
-	glPopMatrix()
-	
-	--hue marker
-	glPushMatrix()
+    glPopMatrix()
+    
+    --selected color box
+    glPushMatrix()
+    glTranslate( colorBarWidthOverall + 2*defaultMargin, -selectedColorBoxY, 0.0 )
+    glColor( curRgbColor.r, curRgbColor.g, curRgbColor.b, 1.0 )
+    glRect( 0.0, 0.0, selectedColorRectWidth, -selectedColorRectWidth )
+    glPopMatrix()
+    
+    --hue marker
+    glPushMatrix()
     glTranslate( defaultMargin + markerX,-colorBarY, 0)
-	glColor( selectorColor )
-	glShape(GL.TRIANGLE_STRIP, { 
-	  { v = { 0, 0 } },
+    glColor( selectorColor )
+    glShape(GL.TRIANGLE_STRIP, { 
+      { v = { 0, 0 } },
       { v = { 0 - colorBarMarkerWidth, colorBarMarkerWidth } },
-	  { v = { 0 + colorBarMarkerWidth, colorBarMarkerWidth } },
-	})
-	glPopMatrix()
-	
-	---saturation / brightness rectangle
-	glPushMatrix()
-	glTranslate( defaultMargin, -satBrightBoxY, 0)
-	local modColor = HSVtoRGB( curHsvColor.h, 1.0, 1.0 )
-	glShape(GL.TRIANGLE_STRIP, {
-      { v = { 0, 0 }, 									c = { 1.0, 1.0, 1.0, colorBarAlpha } },
-      { v = { 0, -satBrightRectWidth }, 				c = { 0.0, 0.0, 0.0, colorBarAlpha } },
-	  { v = { satBrightRectWidth, 0 },					c = { modColor.r, modColor.g, modColor.b, colorBarAlpha } },
-	  { v = { satBrightRectWidth, -satBrightRectWidth },c = { 0.0, 0.0, 0.0, colorBarAlpha } },
+      { v = { 0 + colorBarMarkerWidth, colorBarMarkerWidth } },
+    })
+    glPopMatrix()
+    
+    ---saturation / brightness rectangle
+    glPushMatrix()
+    glTranslate( defaultMargin, -satBrightBoxY, 0)
+    local modColor = HSVtoRGB( curHsvColor.h, 1.0, 1.0 )
+    glShape(GL.TRIANGLE_STRIP, {
+      { v = { 0, 0 },                                   c = { 1.0, 1.0, 1.0, colorBarAlpha } },
+      { v = { 0, -satBrightRectWidth },                 c = { 0.0, 0.0, 0.0, colorBarAlpha } },
+      { v = { satBrightRectWidth, 0 },                  c = { modColor.r, modColor.g, modColor.b, colorBarAlpha } },
+      { v = { satBrightRectWidth, -satBrightRectWidth },c = { 0.0, 0.0, 0.0, colorBarAlpha } },
     })
 
-	--sat/bright selection marker
-	glPushMatrix()
-	glTranslate( satBrightRectWidth * curHsvColor.s, -satBrightRectWidth * (1.0 - curHsvColor.v), 0)
-	glColor(selectorColor)
-	local widthHalf = satBrightMarkerWidth / 2
-	glShape(GL.LINE_LOOP, {
+    --sat/bright selection marker
+    glPushMatrix()
+    glTranslate( satBrightRectWidth * curHsvColor.s, -satBrightRectWidth * (1.0 - curHsvColor.v), 0)
+    glColor(selectorColor)
+    local widthHalf = satBrightMarkerWidth / 2
+    glShape(GL.LINE_LOOP, {
       { v = { 0, satBrightMarkerWidth } },
       { v = { satBrightMarkerWidth, 0 } },
-	  { v = { 0, -satBrightMarkerWidth } },
-	  { v = { -satBrightMarkerWidth, 0 } },
+      { v = { 0, -satBrightMarkerWidth } },
+      { v = { -satBrightMarkerWidth, 0 } },
     })
-	glPopMatrix()
-	--
-	
-	glPopMatrix()
+    glPopMatrix()
+    --
+    
+    glPopMatrix()
 end
 
 function MakeInts( r, g, b )
@@ -196,10 +196,10 @@ function widget:Initialize()
        Spring.GetGameFrame() > 0 then
         widgetHandler:RemoveWidget(self)
     end
-	
-	myTeamId = Spring.GetMyTeamID()
-	
-	UpdateRgb()
+    
+    myTeamId = Spring.GetMyTeamID()
+    
+    UpdateRgb()
 end
 
 function widget:DrawScreen()
@@ -212,56 +212,56 @@ function widget:DrawScreen()
     -- Positioning
     glPushMatrix()
     glTranslate(px, py, 0)
-	
-	glColor(0, 0, 0, 0.5)
+    
+    glColor(0, 0, 0, 0.5)
     glRect( 0, 0, panelWidth, -panelHeight )
-		
-	--color picker
-	DrawColorPicker()
-		
+        
+    --color picker
+    DrawColorPicker()
+        
     glPopMatrix()
 end
 
 function ProcessMouseAction(mx, my, mButton)
     if mx < px or mx > (px + panelWidth) or my > py or my < (py - panelHeight) then
-		return
-	end
+        return
+    end
 
-	-- Spectator check before any action
+    -- Spectator check before any action
     if spGetSpectatingState() then
-		widgetHandler:RemoveWidget(self)
+        widgetHandler:RemoveWidget(self)
         return false
     end
-			
+            
     -- Check buttons
-	if ( mButton == 1 ) then
-		local updated = false
-		if ( mx >= (px + defaultMargin) and mx <= (px + defaultMargin + colorBarWidthOverall) and
-					my <= ( py - colorBarY + colorBarMarkerWidth ) and my >= ( py - colorBarY - colorBarHeight) ) then
-			SetNewHue( (mx - px - defaultMargin) / colorBarWidthOverall )
-			updated = true
-			--Spring.Echo("Color bar click. New hue: " .. curHsvColor.h)
-		elseif ( mx >= (px + defaultMargin) and mx <= (px + defaultMargin + satBrightRectWidth) and
-				my <= ( py - satBrightBoxY ) and my >= ( py - satBrightBoxY - satBrightRectWidth) ) then
-			local newSat = (mx - px - defaultMargin) / satBrightRectWidth
-			local newVal = 1.0 - ((py - my - satBrightBoxY) / satBrightRectWidth)
-			SetNewSatAndVal( newSat, newVal )
-			updated = true
-			--Spring.Echo("SatBright rect click. New sat: " .. newSat .. " NewV: " .. newVal)
-		end
-		
-		if ( updated ) then
-			--send color data to luarules
-			local r,g,b = MakeInts( curRgbColor.r, curRgbColor.g, curRgbColor.b )
-			spSendLuaRulesMsg( "trimColor" .. "," .. r .. "," .. g .. "," .. b )
-		end
-	end
-	
-	return true
+    if ( mButton == 1 ) then
+        local updated = false
+        if ( mx >= (px + defaultMargin) and mx <= (px + defaultMargin + colorBarWidthOverall) and
+                    my <= ( py - colorBarY + colorBarMarkerWidth ) and my >= ( py - colorBarY - colorBarHeight) ) then
+            SetNewHue( (mx - px - defaultMargin) / colorBarWidthOverall )
+            updated = true
+            --Spring.Echo("Color bar click. New hue: " .. curHsvColor.h)
+        elseif ( mx >= (px + defaultMargin) and mx <= (px + defaultMargin + satBrightRectWidth) and
+                my <= ( py - satBrightBoxY ) and my >= ( py - satBrightBoxY - satBrightRectWidth) ) then
+            local newSat = (mx - px - defaultMargin) / satBrightRectWidth
+            local newVal = 1.0 - ((py - my - satBrightBoxY) / satBrightRectWidth)
+            SetNewSatAndVal( newSat, newVal )
+            updated = true
+            --Spring.Echo("SatBright rect click. New sat: " .. newSat .. " NewV: " .. newVal)
+        end
+        
+        if ( updated ) then
+            --send color data to luarules
+            local r,g,b = MakeInts( curRgbColor.r, curRgbColor.g, curRgbColor.b )
+            spSendLuaRulesMsg( "trimColor" .. "," .. r .. "," .. g .. "," .. b )
+        end
+    end
+    
+    return true
 end
 
 function widget:MousePress(mx, my, mButton)
-	return ProcessMouseAction(mx, my, mButton)
+    return ProcessMouseAction(mx, my, mButton)
 end
 
 function widget:MouseMove(mx, my, dx, dy, mButton)
@@ -269,8 +269,8 @@ function widget:MouseMove(mx, my, dx, dy, mButton)
     if mButton == 2 or mButton == 3 then
         px = px + dx
         py = py + dy
-	elseif (mButton == 1 ) then
-		ProcessMouseAction(mx, my, mButton)
+    elseif (mButton == 1 ) then
+        ProcessMouseAction(mx, my, mButton)
     end
 end
 
@@ -279,24 +279,24 @@ function widget:GameStart()
 end
 
 function widget:GetConfigData()
-	local vsx, vsy = gl.GetViewSizes()
-	
-	local data = {}
-	data["position"] = {px / vsx, py / vsy}
-	data["trimcolor"] = curHsvColor
-	return data
+    local vsx, vsy = gl.GetViewSizes()
+    
+    local data = {}
+    data["position"] = {px / vsx, py / vsy}
+    data["trimcolor"] = curHsvColor
+    return data
 end
 
 function widget:SetConfigData(data)
-	if ( data["position"] ~= nil ) then
-		local vsx, vsy = gl.GetViewSizes()
-		px = math.floor(math.max(0, vsx * math.min(data["position"][1] or 0, 0.95)))
-		py = math.floor(math.max(0, vsy * math.min(data["position"][2] or 0, 0.95)))
-	end
-	
-	if ( data["trimcolor"] ~= nil ) then
-		curHsvColor = data["trimcolor"]
-		UpdateRgb()
-	end
+    if ( data["position"] ~= nil ) then
+        local vsx, vsy = gl.GetViewSizes()
+        px = math.floor(math.max(0, vsx * math.min(data["position"][1] or 0, 0.95)))
+        py = math.floor(math.max(0, vsy * math.min(data["position"][2] or 0, 0.95)))
+    end
+    
+    if ( data["trimcolor"] ~= nil ) then
+        curHsvColor = data["trimcolor"]
+        UpdateRgb()
+    end
 end
 

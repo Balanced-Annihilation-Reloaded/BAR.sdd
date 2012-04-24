441be44e8e090274453a329a2999b2be8eeb1ec7
beherith <109391+Beherith@users.noreply.github.com>
2012-04-24 15:06:29 +0000

    BAR: Merge up to commit 366

--- LuaRules/Gadgets/game_end.lua
+++ LuaRules/Gadgets/game_end.lua
@@ -137,7 +137,10 @@ end
 
 local function CheckGameOver()
 	local winners
-	if sharedDynamicAllianceVictory == 0 then
+    
+    if aliveAllyTeamCount == 0 then
+		winners = {}
+	elseif sharedDynamicAllianceVictory == 0 then
 		winners = CheckSingleAllyVictoryEnd()
 	else
 		winners = CheckSharedAllyVictoryEnd()

bd04a8f328218afc57d374449778d0c88ac740c3
nixtux <647338+nixtux@users.noreply.github.com>
2012-06-11 18:46:00 +0000

    BAR: Popup collision volume for cormaw

--- LuaRules/Configs/CollisionVolumes.lua
+++ LuaRules/Configs/CollisionVolumes.lua
@@ -137,6 +137,10 @@ local dynamicPieceCollisionVolume = {}	--dynamic per piece collision volume defi
 		on={64,20,64,0,0,0,1,1,1},
 		off={38,20,38,0,0,0,1,1,1},
 	}
+	unitCollisionVolume["cormaw"] = {
+		on={35,57,35,0,-5,-3,0,1,0},
+		off={35,31,35,0,-5,-3,0,1,0},
+	}
 	unitCollisionVolume["packo"] = {
 		on={49,51,49,-0.5,-10,0,0,1,0},
 		off={49,23,49,-0.5,-10,0,0,1,0},

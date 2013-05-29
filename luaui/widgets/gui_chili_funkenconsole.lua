b0e5b0644d2abfb0f518f46fdfecc4e33bff09e7
Funkencool <Funkencool@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-05-29 19:48:28 +0000

    BAR CHILI: Replaced epic menu with new Option/widget menu, with some other updates

--- luaui/widgets/gui_chili_funkenconsole.lua
+++ luaui/widgets/gui_chili_funkenconsole.lua
@@ -4,12 +4,12 @@ function widget:GetInfo()
 		desc		    = "v0.1 of simple tooltip",
 		author		  = "Funkencool",
 		date		    = "2013",
-		license     = "public domain",
+		license     = "GNU GPL, v2 or later",
 		layer		    = math.huge,
 		enabled   	= true
 	}
 end
-local Chili,window0,stackpanel1,editbox0
+local Chili,window0,stackpanel1,editbox0,whosListening
 local messages = {}
 local box
 local numShowMsg = 1
@@ -20,7 +20,15 @@ local function updateMsgWindow()
 	window0:ClearChildren()
 	local text = ''
 	for i=1, numShowMsg do
-		if messages[#messages-numShowMsg+i] then text = text..messages[#messages-numShowMsg+i].text..'\n' end
+		local msg = messages[#messages-numShowMsg+i]
+		if msg then 
+			-- if msg.source then text = text..msg.source..", " end
+			-- if msg.msgtype then text = text..msg.msgtype..", " end
+			-- if msg.player then text = text..msg.player..", " end
+			if string.find(msg.text, "Local") then text=text.."\255\255\127\0" end
+			text = text..msg.text..'\n'
+			if string.find(msg.text, "Local") then text=text.."\b" end
+		end
 	end
 	box = Chili.TextBox:New{name='console',width=300,text=text}
 	local wText = box.font:WrapText(text, 300, 500)
@@ -31,8 +39,9 @@ end
 
 function widget:Initialize()
 	Chili = WG.Chili
-	editbox0 = Chili.EditBox:New{parent=Chili.Screen0,right=250,y=20,height=30,width=350}
-	window0 = Chili.Window:New{parent = Chili.Screen0,padding = {5,0,5,0},minHeight = 15,right = 250,y = 50,width = 300}
+	local screen = Chili.Screen0
+	editbox0 = Chili.EditBox:New{parent=screen,right=300,y=20,height=30,width=300}
+	window0 = Chili.Window:New{parent=screen,padding = {5,0,5,0},minHeight = 15,right = 250,y = 50,width = 300}
 	
 	local buffer = Spring.GetConsoleBuffer(200)
 	for i=1,#buffer do
@@ -40,7 +49,8 @@ function widget:Initialize()
 	end
 	
 	Spring.SendCommands("console 0")
-	Spring.SendCommands("inputtextgeo "..(editbox0.x/Chili.Screen0.width+0.004)..' '..(1 - (editbox0.y + editbox0.height) / Chili.Screen0.height + 0.003)..' 0.02 '..(editbox0.width / Chili.Screen0.width))
+	Spring.SendCommands("inputtextgeo "..(editbox0.x/Chili.Screen0.width+0.004)..' '
+		..(1 - (editbox0.y + editbox0.height) / Chili.Screen0.height + 0.003)..' 0.02 '..(editbox0.width / Chili.Screen0.width))
 end
 
 function widget:GameFrame(n)
@@ -51,12 +61,19 @@ function widget:GameFrame(n)
 end
 
 function widget:AddConsoleMessage(msg)
-	messages[#messages+1]=msg
+	messages[#messages+1] = {}
+	messages[#messages]=msg
 	updateMsgWindow()
 	if numShowMsg < 6 then numShowMsg = numShowMsg+1 end
 end
-
+function widget:KeyPress(key)
+	if key==13 and editbox0.state.focused then
+		Spring.SendCommands('Say '..editbox0.text)
+		editbox0.text = ''
+	return true
+	end
+end
 function widget:Shutdown()
-	Spring.SendCommands({"console 1", "inputtextgeo default"}) -- not saved to spring's config file on exit
-	Spring.SetConfigString("InputTextGeo", "0.26 0.73 0.02 0.028") -- spring default values
+	Spring.SendCommands({"console 1", "inputtextgeo default"}) 
+	Spring.SetConfigString("InputTextGeo", "0.26 0.73 0.02 0.028")
 end

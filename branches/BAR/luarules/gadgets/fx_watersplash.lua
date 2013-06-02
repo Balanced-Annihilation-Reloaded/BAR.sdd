1b61882aab1df03599bfe362078344cef2b92c33
nixtux <nixtux@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-06-02 07:04:41 +0000

    BAR: Add improved watersplash cegs,fx_watersplash gadget now has separate config file with blocklist (for future use)

--- branches/BAR/luarules/gadgets/fx_watersplash.lua
+++ branches/BAR/luarules/gadgets/fx_watersplash.lua
@@ -2,7 +2,7 @@
 function gadget:GetInfo()
   return {
     name      = "Watereffects",
-    desc      = "Make splash sound in water",
+    desc      = "Spawns splash effect",
     version   = "1.1",
     author    = "Jools ,Nixtux",
     date      = "April,2012",
@@ -15,15 +15,12 @@ end
 LUAUI_DIRNAME= 'LuaUI/'
 local random  = math.random
 local abs = math.abs
+local SpGetGroundHeight = Spring.GetGroundHeight
 local GetGroundBlocked = Spring.GetGroundBlocked
-local nonexplosiveWeapons = {
-	LaserCannon = true,
-	BeamLaser = true,
-	EmgCannon = true,
-	Flame = true,
-	LightningCannon = true,
-}
+local SpSpawnCEG = Spring.SpawnCEG
 
+local nonexplosiveWeapons, BlackList = VFS.Include"LuaRules/Configs/fx_watersplash_config.lua"
+   
 if not gadgetHandler:IsSyncedCode() then
 	-------------------
 	-- UNSYNCED PART --
@@ -41,30 +38,29 @@ else
 
 	
 	function gadget:Explosion(weaponID, px, py, pz, ownerID)
-		local isWater = Spring.GetGroundHeight(px,pz) < 0
+		local isWater = SpGetGroundHeight(px,pz) < 0
 		local aoe = WeaponDefs[weaponID]["damageAreaOfEffect"] / 2
 		local wType = WeaponDefs[weaponID].type
-				if not nonexplosiveWeapons[wType] and isWater and abs(py) <= aoe and (not GetGroundBlocked(px, pz)) then
-			if aoe >= 8 and aoe < 16 then
-				Spring.SpawnCEG(splashCEG1, px, 0, pz)
+		local wdName = WeaponDefs[weaponID].name
+		if not (nonexplosiveWeapons[wType]) and not (BlackList[wdName]) and isWater and (abs(py) <= aoe) and (not GetGroundBlocked(px, pz)) then
+		        if aoe >= 8 and aoe < 16 then
+				SpSpawnCEG(splashCEG1, px, 0, pz)
 			elseif aoe >= 16 and aoe < 48 then
-				Spring.SpawnCEG(splashCEG2, px, 0, pz)
+				SpSpawnCEG(splashCEG2, px, 0, pz)
 			elseif aoe >= 48 and aoe < 64 then
-				Spring.SpawnCEG(splashCEG3, px, 0, pz)
+				SpSpawnCEG(splashCEG3, px, 0, pz)
 			elseif aoe >= 64 and aoe < 300 then
-				Spring.SpawnCEG(splashCEG4, px, 0, pz)
+				SpSpawnCEG(splashCEG4, px, 0, pz)
 			end
-			return true
-		else
-			return false
 		end
+		return false
 	end
 	
 	function gadget:Initialize()
 	
 		for id,Def in pairs(WeaponDefs) do
 			local weaponID
-			if Def.damageAreaOfEffect ~= nil and Def.damageAreaOfEffect >8 and not nonexplosiveWeapons[Def.type] then
+			if Def.damageAreaOfEffect ~= nil and Def.damageAreaOfEffect > 8 and not nonexplosiveWeapons[Def.type] then
 				Script.SetWatchWeapon(Def.id, true)
 			end
 		end

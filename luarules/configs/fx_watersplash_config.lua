c9c5baa13758f8b52cd7703d554e77ed7a12fcde
nixtux <647338+nixtux@users.noreply.github.com>
2013-06-02 07:04:41 +0000

    BAR: Add improved watersplash cegs,fx_watersplash gadget now has separate config file with blocklist (for future use)

---
+++ luarules/configs/fx_watersplash_config.lua
@@ -0,0 +1,13 @@
+local nonexplosiveWeapons = {
+     LaserCannon = true,
+     BeamLaser = true,
+     EmgCannon = true,
+     Flame = true,
+     LightningCannon = true,
+     DGun = true,
+}
+
+local BlackList = {
+}
+   
+return nonexplosiveWeapons, BlackList

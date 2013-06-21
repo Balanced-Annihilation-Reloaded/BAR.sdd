4ec7a68eecb751c43349c9b327a5eee090d19f41
Funkencool <Funkencool@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-06-21 01:43:41 +0000

    BAR CHILI: Redid cursortip a little and applied delay

--- branches/BAR/luaui/widgets/gui_chili_funkentooltip.lua
+++ branches/BAR/luaui/widgets/gui_chili_funkentooltip.lua
@@ -1,58 +1,67 @@
 function widget:GetInfo()
-	return {
-		name		    = "BAR's funken tooltip",
-		desc		    = "v0.1 of simple tooltip",
-		author		  = "Funkencool",
-		date		    = "2013",
-		license     = "public domain",
-		layer		    = math.huge,
-		enabled   	= true
-	}
+ return {
+  name      = "BAR's funken cursortip",
+  desc      = 'v0.1 of a simple tooltip',
+  author    = 'Funkencool',
+  date      = '2013',
+  license     = 'public domain',
+  layer      = math.huge,
+  enabled    = true
+ }
 end
 
---Chili elements
-local Chili, cursortip, screen0
-local varVar = 0
+local Chili, screen0, tipWindow, tip
 local mousePosX, mousePosY
-local spGetCurrentTooltip = Spring.GetCurrentTooltip
-local spTraceScreenRay 		= Spring.TraceScreenRay
-local spGetMouseState			= Spring.GetMouseState
-local spGetUnitTooltip = Spring.GetUnitTooltip
-local tooltip = ""
+local showFrame
+local tooltip = ''
+
+local spTraceScreenRay          = Spring.TraceScreenRay
+local spGetMouseState           = Spring.GetMouseState
+local spGetUnitTooltip          = Spring.GetUnitTooltip
 local screenWidth, screenHeight = Spring.GetWindowGeometry()
 
-
+-----------------------------------
 local function initWindow()
-	control0	= Chili.Window:New{parent = screen0, width = 300, height = 75, padding = {5,0,0,0},minHeight=1}
-	tip				= Chili.TextBox:New{parent = control0, x = 0, y = 0, right = 0, bottom = 0,margin = {0,0,0,0}}
+ tipWindow = Chili.Window:New{parent = screen0, width = 300, height = 75, padding = {5,0,0,0},minHeight=1}
+ tip = Chili.TextBox:New{parent = tipWindow, x = 0, y = 0, right = 0, bottom = 0,margin = {0,0,0,0}}
 end
-
-local function MakeToolTip(tooltip,x,y)
-	if tooltip ~= '' then 
-		local textwidth = tip.font:GetTextWidth(tooltip)
-		local textheight,_,numLines = tip.font:GetTextHeight(tooltip)
-		control0:SetPos(x + 20, screenHeight - y + 20,textwidth+10,14*numLines+2)
-		if control0.hidden then control0:Show() end
-		control0:BringToFront()
-	elseif control0.visible then control0:Hide() end
-	tip:SetText(tooltip)
+-----------------------------------
+local function getTooltip()
+ mousePosX, mousePosY   = spGetMouseState()
+ local typeOver, ID     = spTraceScreenRay(mousePosX, mousePosY)
+ if screen0.currentTooltip then tooltip = screen0.currentTooltip
+ elseif typeOver == 'unit' then tooltip = spGetUnitTooltip(ID)
+ else                           tooltip = '' 
+ end
 end
-
-function widget:DrawScreen()
-	local mousePosX, mousePosY = spGetMouseState()
-	local typeOver, ID = spTraceScreenRay(mousePosX, mousePosY)
-	if screen0.currentTooltip then tooltip = screen0.currentTooltip
-	elseif typeOver == "unit" then tooltip = spGetUnitTooltip(ID)
-	else tooltip = '' end
-	if tip.text ~= tooltip then
-		MakeToolTip(tooltip,mousePosX, mousePosY)
-	end
+-----------------------------------
+local function MakeToolTip()
+ local tooltip               = tooltip
+ local x,y                   = mousePosX,mousePosY
+ local textwidth             = tip.font:GetTextWidth(tooltip)
+ local textheight,_,numLines = tip.font:GetTextHeight(tooltip)
+ tipWindow:SetPos(x + 20, screenHeight - y + 20,textwidth+10,14*numLines+2)
+ if tipWindow.hidden then tipWindow:Show() end
+ tipWindow:BringToFront()
 end
-
-
+-----------------------------------
+function widget:GameFrame(frame)
+ getTooltip()
+ if tip.text ~= tooltip then
+  showFrame = frame + 20
+  tip:SetText(tooltip)
+  if tipWindow.visible then tipWindow:Hide() end
+ elseif showFrame < frame and tooltip ~= '' then
+  MakeToolTip()
+ end
+end
+-----------------------------------
 function widget:Initialize()
-	if not WG.Chili then return end
-	Chili = WG.Chili
-	screen0 = Chili.Screen0
-	initWindow()
+ if not WG.Chili then return end
+ Chili = WG.Chili
+ screen0 = Chili.Screen0
+ initWindow()
+end
+function widget:Shutdown()
+  tipWindow:Dispose()
 end

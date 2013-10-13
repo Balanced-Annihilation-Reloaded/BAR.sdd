e4cde65483f85912f408668e396d090e1a1c8847
beherith <109391+Beherith@users.noreply.github.com>
2013-10-13 13:11:26 +0000

    BAR: Update shader for future line style lights

--- shaders/deferred_lighting.glsl
+++ shaders/deferred_lighting.glsl
@@ -6,6 +6,9 @@
   uniform sampler2D tex0;
   uniform vec3 eyePos;
   uniform vec4 lightpos;
+  #ifdef BAR_LIGHT
+	uniform vec4 lightpos2;
+  #endif
   uniform vec4 lightcolor;
   uniform vec4 lightparams;
   uniform mat4 viewProjectionInv;
@@ -30,11 +33,54 @@
 	
 	vec4 herenormal4;
 	herenormal4.xyz = -1.0*normalize(cross( up4.xyz - here4.xyz, right4.xyz - here4.xyz));
-	float dist_light_here = length(lightpos.xyz - here4.xyz);
-	float cosphi = max(0.0 , dot (herenormal4.xyz, lightpos.xyz - here4.xyz) / dist_light_here);
-	//float attentuation =  max(0, ( 1.0 - (dist_light_here)/(lightpos.w)) ); // pretty good function, but its peak is too sharp, especially for lasers. https://www.desmos.com/calculator/vyc3ulbzj6
-	//float attentuation =  max( 0,( 1.0 - (dist_light_here*dist_light_here)/(lightpos.w*lightpos.w)) );
-	float attentuation =  max( 0,( lightparams.r - lightparams.g * (dist_light_here*dist_light_here)/(lightpos.w*lightpos.w) - lightparams.b*(dist_light_here)/(lightpos.w)) );
+	#ifndef BAR_LIGHT
+	//gl_FragColor=vec4(1,0,0,0.5);
+	//return;
+		float dist_light_here = length(lightpos.xyz - here4.xyz);
+		float cosphi = max(0.0 , dot (herenormal4.xyz, lightpos.xyz - here4.xyz) / dist_light_here);
+		//float attentuation =  max(0, ( 1.0 - (dist_light_here)/(lightpos.w)) ); // pretty good function, but its peak is too sharp, especially for lasers. https://www.desmos.com/calculator/vyc3ulbzj6
+		//float attentuation =  max( 0,( 1.0 - (dist_light_here*dist_light_here)/(lightpos.w*lightpos.w)) );
+		float attentuation =  max( 0,( lightparams.r - lightparams.g * (dist_light_here*dist_light_here)/(lightpos.w*lightpos.w) - lightparams.b*(dist_light_here)/(lightpos.w)) );
+	#endif
+	#ifdef BAR_LIGHT
+	gl_FragColor=vec4(1,0,1,0.5);
+	return;
+		//def dist(x1,y1, x2,y2, x3,y3): # x3,y3 is the point
+		/*distance( Point P,  Segment P0:P1 ) // http://geomalgorithms.com/a02-_lines.html
+		{
+			   v = P1 - P0
+			   w = P - P0
+
+			   if ( (c1 = w dot v) <= 0 )  // before P0
+					   return d(P, P0)
+			   if ( (c2 = v dot v) <= c1 ) // after P1
+					   return d(P, P1)
+
+			   b = c1 / c2
+			   Pb = P0 + bv
+			   return d(P, Pb)
+		}
+		*/
+
+		vec3 v=lightpos2.xyz-lightpos.xyz;
+		vec3 w=here4.xyz-lightpos.xyz;
+		float c1=dot(v,w);
+		float c2=dot(v,v);
+		if (c1<=0.0){
+			v=here4.xyz;
+			w=lightpos.xyz;
+		}else if (c2<c1){
+			v=here4.xyz;
+			w=lightpos2.xyz;
+		}else{
+			w=lightpos.xyz+(c1/c2)*v;
+			v=here4.xyz;
+		}
+		float dist_light_here = length(v-w);
+		float cosphi = max(0.0 , dot (herenormal4.xyz, w.xyz - here4.xyz) / dist_light_here);
+		float attentuation =  max( 0,( lightparams.r - lightparams.g * (dist_light_here*dist_light_here)/(lightpos.w*lightpos.w) - lightparams.b*(dist_light_here)/(lightpos.w)) );
+	#endif
+
 	
 	//lightparams info:
 	// linear funcion is (1,0,1)
@@ -42,9 +88,14 @@
 	// tits function (for lasers, to avoid hotspotting) (0.5, 2, -1.5)
 	attentuation *=attentuation;
 	
+	//TODO:
+	//add a specular highlight to the lighting with eyepos
+	
 	//gl_FragColor=vec4(normalize(herenormal4.xyz), cosphi*(lightpos.w/(dist_light_here*dist_light_here)));
 	//if (attentuation > 0.001) gl_FragColor(1,0,0,1);
 	//else gl_FragColor(0,1,0,1);
+	// gl_FragColor=vec4(1,1,0,0.5);
+	// return;
 	gl_FragColor=vec4(lightcolor.rgb, cosphi*attentuation);
 	return;
   }

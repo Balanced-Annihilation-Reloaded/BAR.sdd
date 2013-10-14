31c3fed1916513aa47124ff83e11dc11f9de46ad
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-10-14 13:59:40 +0000

    BAR: made funks_sinfo cope with >9 UnitDefIDs in selection      added numbers onto healthbar when single unit is selected      display coord of map point when no unit is selected

--- branches/BAR/luaui/widgets/gui_chili_funks_sInfo.lua
+++ branches/BAR/luaui/widgets/gui_chili_funks_sInfo.lua
@@ -14,20 +14,28 @@ end
 
 local imageDir = 'luaui/images/buildIcons/'
 
-local Chili ,infoWindow, unitInfo, unitName, unitIcon, selectionGrid, unitHealth, unitProg
+local Chili ,infoWindow, unitInfo, unitName, unitIcon, selectionGrid, unitHealthText, unitHealth, groundInfo 
 local healthBars = {}
-local updateNow
+
+local updateNow = false
+local curTip --[[ current tooltip type: -3 for ground info
+									    -2 for so many unitDefIDs that we just give text info 
+										-1 for multiple unitDefIDs that fit with pics (<=9)
+										>=0 for a single unit & is the unitID  ]]
 
 local spGetUnitDefID            = Spring.GetUnitDefID
 local spGetUnitTooltip          = Spring.GetUnitTooltip
 local spGetSelectedUnits        = Spring.GetSelectedUnits
 local spGetUnitHealth           = Spring.GetUnitHealth
 local spGetSelectedUnitsSorted  = Spring.GetSelectedUnitsSorted
+local spGetMouseState			= Spring.GetMouseState
+local spTraceScreenRay			= Spring.TraceScreenRay
 
 local r,g,b     = Spring.GetTeamColor(Spring.GetMyTeamID())
 local teamColor = {r,g,b}
 
 ----------------------------------
+-- add unitDefID (curTip = -1)
 local function addUnitGroup(name,texture,overlay,unitIDs)
 	local count = #unitIDs
 	if count == 1 then count = '' end
@@ -74,14 +82,21 @@ local function addUnitGroup(name,texture,overlay,unitIDs)
 end
 
 ----------------------------------
-local function showUnitInfo(texture, overlay, description, humanName)
+-- unit info (curTip >= 0)
+local function showUnitInfo(texture, overlay, description, humanName, health, maxHealth)
 	
 	unitName = Chili.TextBox:New{
 		x      = 0,
 		y      = 0,
 		right  = 0,
 		bottom = 0,
-		text   = humanName..'\n'..description,
+		text   = " " .. humanName..'\n'.. " " .. description,
+	}
+	
+	unitHealthText = Chili.TextBox:New{
+		x      = 5,
+		bottom = 21,
+		text   = math.floor(health) ..' / '.. math.floor(maxHealth),
 	}
 	
 	unitHealth = Chili.Progressbar:New{
@@ -104,7 +119,7 @@ local function showUnitInfo(texture, overlay, description, humanName)
 				height   = '100%',
 				width    = '100%',
 				file     = overlay,
-				children = {unitName, unitHealth},
+				children = {unitName, unitHealthText, unitHealth},
 			}
 		}
 	}
@@ -113,39 +128,85 @@ local function showUnitInfo(texture, overlay, description, humanName)
 	unitInfo:AddChild(unitIcon)
 	
 end
-
+----------------------------------
+-- ground info (curTip = -3)
+local function showGroundInfo(text)
+	
+	groundText = Chili.TextBox:New{
+		x      = 0,
+		y      = 0,
+		right  = 0,
+		bottom = 0,
+		text   = text,
+	}
+	
+	unitInfo:AddChild(groundText)
+end
+----------------------------------
+-- text unit info only (curTip = -2)
+local function showBasicUnitInfo(num, numTypes)
+	
+	basicUnitInfo = Chili.TextBox:New{
+		x      = 0,
+		y      = 0,
+		right  = 0,
+		bottom = 0,
+		text   = " units: " .. num,
+	}
+	
+	unitInfo:AddChild(basicUnitInfo)
+end
 ----------------------------------
 local function getInfo()
 	
-	local sortedUnits = spGetSelectedUnitsSorted()
-	local units = spGetSelectedUnits()
+	units = spGetSelectedUnits()
 	
-	if #units == 1 then
-		
+	if #units == 0 then
+		--info about point on map corresponding to cursor (updated every other gameframe)
+		curTip = -3
+		showGroundInfo("")
+
+	elseif #units == 1 then
+		--detailed info about a single unit
 		local unitID      = units[1]
+		curTip = unitID
 		local defID       = spGetUnitDefID(unitID)
 		local description = UnitDefs[defID].tooltip or ''
 		local name        = UnitDefs[defID].name
 		local texture     = imageDir..'Units/' .. name .. '.png'
 		local overlay     = imageDir..'Overlays/' .. name .. '.png'
 		local humanName   = UnitDefs[defID].humanName
+		local curHealth, maxHealth = spGetUnitHealth(unitID)
+	
+		showUnitInfo(texture, overlay, description, humanName, curHealth, maxHealth)
 		
-		unitProg = unitID
-		showUnitInfo(texture, overlay, description, humanName)
-		
-		else
-		
-		for defID, unitIDs in pairs(sortedUnits) do
-			if defID == 'n' then break end
+	else
+		--broad info about lots of units
+		curTip = -1
+		local sortedUnits = spGetSelectedUnitsSorted()
+		local unitDefIDCount = 0
+		local unitCount = 0
+			--see if sortedUnits has too many elements (believe it or not, this is the only way to count an arbitrary table...)
+			for unitDefID,unitIDs in pairs(sortedUnits) do
+				if unitDefID ~= 'n' then  --i don't know why but it always has an 'n' key with nil value in the table, wtf?
+					unitDefIDCount = unitDefIDCount + 1
+				end
+				if unitDefIDCount == 10 then break end
+			end
 			
-			local name    = UnitDefs[defID].name
-			local texture = imageDir..'Units/' .. name .. '.png'
-			local overlay = imageDir..'Overlays/' .. name .. '.png'
-			
-			addUnitGroup(name,texture,overlay,unitIDs)
-			
-		end
-		
+			if unitDefIDCount <= 9 then 
+				--pics & healthbars, grouped by UnitDefID, if it fits
+				for unitDefID, unitIDs in pairs(sortedUnits) do
+					if unitDefID ~= 'n' then 
+						local name    = UnitDefs[unitDefID].name
+						local texture = imageDir..'Units/' .. name .. '.png'
+						local overlay = imageDir..'Overlays/' .. name .. '.png'
+						addUnitGroup(name,texture,overlay,unitIDs)
+					end
+				end
+			else
+				showBasicUnitInfo(#units)
+			end
 	end
 	
 end
@@ -165,7 +226,7 @@ function widget:Initialize()
 	screen0 = Chili.Screen0
 	winSize = screen0.height * 0.2
 	
-	--Main window, parent of everything
+	--Main window, ancestor of everything
 	infoWindow = Chili.Window:New{
 		padding = {0,0,0,0},
 		parent  = Chili.Screen0,
@@ -197,12 +258,23 @@ function widget:Initialize()
 		margin  = {0,0,0,0},
 	}
 	
+	groundInfo = Chili.Control:New{
+		parent  = infoWindow,
+		x       = 0,
+		y       = 0,
+		height  = '100%',
+		width   = '100%',
+		padding = {0,0,0,0},
+		margin  = {0,0,0,0},
+	}
+	
 	Spring.SetDrawSelectionInfo(false)
+
 end
 
 ----------------------------------
 function widget:CommandsChanged()
-	unitProg = nil
+	curTip = nil
 	healthBars = {}
 	selectionGrid:ClearChildren()
 	unitInfo:ClearChildren()
@@ -216,29 +288,47 @@ function widget:Shutdown()
 	Spring.SetDrawSelectionInfo(true)
 end
 
+
 function widget:GameFrame(n)
-	if (n % 30 < 1) or updateNow then
-		if unitProg then
-			
-			local health, maxhealth = spGetUnitHealth(unitProg)
-			unitHealth:SetMinMax(0,maxhealth)
-			unitHealth:SetValue(health)
-			
-			elseif #healthBars > 0 then
-			for a=1, #healthBars do
-				local value, max = 0, 0
-				for b=1, #healthBars[a].unitIDs do
-					local health, maxhealth = spGetUnitHealth(healthBars[a].unitIDs[b])
-					max   = max + maxhealth
-					value = value + health
-				end
-				healthBars[a].max = max
-				healthBars[a]:SetValue(value)
-			end
+	if n % 2 ~= 0 then return end
+	
+	if curTip == -3 then --ground
+		local mx, my = spGetMouseState()
+		local focus,map = spTraceScreenRay(mx,my)
+		if focus == "ground" and map[1] then
+			local px,py,pz = math.floor(map[1]),math.floor(map[2]),math.floor(map[3])
+			groundText:SetText(" pos: " .. px ..  ", " .. pz .. "\n" .. " height: " .. py)
 		end
-		
+		return
+	end
+	
+	if not updateNow and n % 12 ~= 0 then return end
+	
+	if curTip >= 0 then --single unit
+		local health, maxHealth = spGetUnitHealth(curTip)
+		unitHealthText:SetText(math.floor(health) ..' / '.. math.floor(maxHealth)) 
+		unitHealthText:Invalidate() --not sure why this is needed here but it is
+		unitHealth:SetMinMax(0,maxHealth)
+		unitHealth:SetValue(health)
+		updateNow = false
+		return		
+	end
+			
+	if curTip == -1 then --multiple units, but not so many we cant fit pics
+		for a=1, #healthBars do
+			local value, max = 0, 0
+			for b=1, #healthBars[a].unitIDs do
+				local health, maxhealth = spGetUnitHealth(healthBars[a].unitIDs[b])
+				max   = max + maxhealth
+				value = value + health
+			end
+			healthBars[a].max = max
+			healthBars[a]:SetValue(value)
+		end		
 		updateNow = false
 	end
+	
+	--no need to update anything for curTip = -2
 end
 
 

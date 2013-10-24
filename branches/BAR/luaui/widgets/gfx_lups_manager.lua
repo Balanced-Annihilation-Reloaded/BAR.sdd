2e927ae67d7267ca0fb29e5ed35bc415518fdb19
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-10-24 17:46:05 +0000

    BAR: aafus lups was too pricey, had to go, especially those tiny glowballs, wtf?

--- branches/BAR/luaui/widgets/gfx_lups_manager.lua
+++ branches/BAR/luaui/widgets/gfx_lups_manager.lua
@@ -102,23 +102,25 @@ local UnitEffects = {
     {class='GroundFlash',options=groundFlashGreen},
   },
   [UnitDefNames["aafus"].id] = {
-    {class='SimpleParticles2', options=MergeTable(sparks,{pos={-36,82,-36}, delay=10, lifeSpread=30, partpos="0,0,0"})},
-    {class='SimpleParticles2', options=MergeTable(sparks,{pos={36,82,-36}, delay=60, lifeSpread=30, partpos="0,0,0"})},
-    {class='SimpleParticles2', options=MergeTable(sparks,{pos={36,82,36}, delay=30, lifeSpread=30, partpos="0,0,0"})},
-    {class='SimpleParticles2', options=MergeTable(sparks,{pos={-36,82,36}, delay=90, lifeSpread=30, partpos="0,0,0"})},
+  
+  --TOO EXPENSIVE:
+   --{class='SimpleParticles2', options=MergeTable(sparks,{pos={-36,82,-36}, delay=10, lifeSpread=30, partpos="0,0,0"})},
+    --{class='SimpleParticles2', options=MergeTable(sparks,{pos={36,82,-36}, delay=60, lifeSpread=30, partpos="0,0,0"})},
+    --{class='SimpleParticles2', options=MergeTable(sparks,{pos={36,82,36}, delay=30, lifeSpread=30, partpos="0,0,0"})},
+   --{class='SimpleParticles2', options=MergeTable(sparks,{pos={-36,82,36}, delay=90, lifeSpread=30, partpos="0,0,0"})},
 
-	{class='Sphere', options=MergeTable(aafusGlowBallWhite,{pos={31.2,32.7,0}})},
-	{class='Sphere', options=MergeTable(aafusGlowBallWhite,{pos={-31.2,32.7,0}})},
-	{class='Sphere', options=MergeTable(aafusGlowBallWhite,{pos={0,32.7,31.2}})},
-	{class='Sphere', options=MergeTable(aafusGlowBallWhite,{pos={0,32.7,-31.2}})},
+	--{class='Sphere', options=MergeTable(aafusGlowBallWhite,{pos={31.2,32.7,0}})},
+	--{class='Sphere', options=MergeTable(aafusGlowBallWhite,{pos={-31.2,32.7,0}})},
+	--{class='Sphere', options=MergeTable(aafusGlowBallWhite,{pos={0,32.7,31.2}})},
+	--{class='Sphere', options=MergeTable(aafusGlowBallWhite,{pos={0,32.7,-31.2}})},
 
-	{class='Sphere', options=MergeTable(aafusGlowBallTeal,{pos={28.2,36,28.2}})},
-	{class='Sphere', options=MergeTable(aafusGlowBallTeal,{pos={-28.2,36,28.2}})},
-	{class='Sphere', options=MergeTable(aafusGlowBallTeal,{pos={-28.2,36,-28.2}})},
-	{class='Sphere', options=MergeTable(aafusGlowBallTeal,{pos={28.2,36,-28.2}})},
+	--{class='Sphere', options=MergeTable(aafusGlowBallTeal,{pos={28.2,36,28.2}})},
+	--{class='Sphere', options=MergeTable(aafusGlowBallTeal,{pos={-28.2,36,28.2}})},
+	--{class='Sphere', options=MergeTable(aafusGlowBallTeal,{pos={-28.2,36,-28.2}})},
+	--{class='Sphere', options=MergeTable(aafusGlowBallTeal,{pos={28.2,36,-28.2}})},
 
-    {class='Bursts', options={pos={0,76,0}, size=35, sizeSpread=2, colormap={{1.0,0.2,0.2,0.4}}, life=20, arc=90, rotSpeed=0.7, rotSpread=1, count=43, directional=true, repeatEffect=true}},
-	{class='ShieldSphere', options={pos={0,76,0}, size=29, colormap1={ {1.0,0.0,0.0,0.5} }, colormap2={ {1.0,0.3,0.3,0.8},{1.0,0.0,0.0,0.5},{1.0,0.3,0.3,0.8} }, life=20, repeatEffect=true}},
+   -- {class='Bursts', options={pos={0,76,0}, size=35, sizeSpread=2, colormap={{1.0,0.2,0.2,0.4}}, life=20, arc=90, rotSpeed=0.7, rotSpread=1, count=43, directional=true, repeatEffect=true}},
+	{class='ShieldSphere', options={pos={0,76,0}, size=26, colormap1={ {1.0,0.0,0.0,0.5} }, colormap2={ {1.0,0.3,0.3,0.8},{1.0,0.0,0.0,0.5},{1.0,0.3,0.3,0.8} }, life=20, repeatEffect=true}},
     {class='ShieldJitter', options={layer=-16, life=20, pos={0,76,0}, size=29.7, precision=22, repeatEffect=true}},
   },
   [UnitDefNames["corgate"].id] = {

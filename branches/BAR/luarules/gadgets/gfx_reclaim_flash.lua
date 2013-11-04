7fe12be0983695247a3e7360776e8e5458096ed4
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-11-04 20:51:03 +0000

    BAR: added a CEG effect to resurrection

--- branches/BAR/luarules/gadgets/gfx_reclaim_flash.lua
+++ branches/BAR/luarules/gadgets/gfx_reclaim_flash.lua
@@ -14,16 +14,41 @@ if (not gadgetHandler:IsSyncedCode()) then
   return
 end
 
+local GetFeaturePosition 	= Spring.GetFeaturePosition
+local GetFeatureResources 	= Spring.GetFeatureResources
+local SpawnCEG				= Spring.SpawnCEG
+local GetUnitDefID 			= Spring.GetUnitDefID
+local GetUnitPosition		= Spring.GetUnitPosition
+
+local rezzers={
+	[UnitDefNames.armrectr.id] = true,
+	[UnitDefNames.armrecl.id] = true,
+	[UnitDefNames.cornecro.id] = true,
+	[UnitDefNames.correcl.id] = true,
+}
+
 function gadget:FeatureDestroyed(featureID,allyteam)
-	fx,fy,fz=Spring.GetFeaturePosition(featureID)
+	local fx,fy,fz=GetFeaturePosition(featureID)
 	--Spring.Echo(allyteam)
 	if (fx ~= nil) then
-		rm, mm, re, me, rl = Spring.GetFeatureResources(featureID)
+		rm, mm, re, me, rl = GetFeatureResources(featureID)
 		if (rm ~= nil) then
 			if mm==0 and re == 0 then
-				Spring.SpawnCEG("sparklegreen", fx, fy, fz)
+				SpawnCEG("sparklegreen", fx, fy, fz,1,0,1)
 			end
 		end
 	end
 end
 
+function gadget:UnitCreated(unitID, unitDefID, teamID, builderID)
+
+	--Spring.Echo('resurrection!', builderID)
+	
+	if (builderID and builderID > 0 and rezzers[GetUnitDefID(builderID)]) then
+		local px,py,pz=GetUnitPosition(unitID)
+		if px~=nil then
+			SpawnCEG("sparklegreeninverse",px,py,pz)
+		end
+		--Spring.Echo('resurrection!')
+	end
+end

bdfada23bdb5b46322e0bf4b087500d08be65d67
Funkencool <Funkencool@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2013-11-13 04:26:16 +0000

    Bar - updated console to work when paused, plus some clean up

--- luaui/widgets/gui_chili_funks_console.lua
+++ luaui/widgets/gui_chili_funks_console.lua
@@ -11,87 +11,137 @@ function widget:GetInfo()
 	}
 end
 
-local Chili,conOut,editbox0,whosListening,control0,window0
+-- Spring Functions --
+local getTimer         = Spring.GetTimer
+local diffTimers       = Spring.DiffTimers
+local sendCommands     = Spring.SendCommands
+local setConfigString  = Spring.SetConfigString
+local getConsoleBuffer = Spring.GetConsoleBuffer
+----------------------
+
+
+-- Config --
+local maxMsgNum = 6
+local msgTime   = 4 -- time to display message in seconds
+local msgWidth  = 300
+------------
+
+-- Chili elements --
+local Chili
+local msgWindow
+local editbox0
+local whosListening
+local control0
+local window0
+local textBox
+--------------------
+
+-- Local Variables --
 local messages = {}
-local box
-local numShowMsg = 1
-local windowHeight = 1
+local curMsgNum = 1
+local timer = getTimer()
+local oldTimer = timer
+---------------------
 
 local function updateMsgWindow()
-	conOut:ClearChildren()
 	local text = ''
-	for i=1, numShowMsg do
-		local msg = messages[#messages-numShowMsg+i]
-		if msg then
-			-- if msg.source then text = text..msg.source..', ' end
-			-- if msg.msgtype then text = text..msg.msgtype..', ' end
-			-- if msg.player then text = text..msg.player..', ' end
-			if string.find(msg.text, 'Local') then text=text..'\255\255\127\0' end
-			text = text..msg.text..'\n'
-			if string.find(msg.text, 'Local') then text=text..'\b' end
-		end
+	
+	for i=1, curMsgNum do
+		local msg = messages[#messages-curMsgNum+i]
+		if msg then text = text..msg.text..'\n' end
 	end
-	box = Chili.TextBox:New{name='console',width=300,text=text}  --inefficient to make a new one of these every time TODO: don't clear child and update text
-	local wText = box.font:WrapText(text, 300, 500)
-	_,_,box.lines = box.font:GetTextHeight(wText) 
-	conOut:Resize(300,math.max(0,(box.lines-1))*15)
-	conOut:AddChild(box)
+	
+	textBox:SetText(text)
+	local wText, lines = textBox.font:WrapText(text, msgWidth, 500)
+	msgWindow:Resize(msgWidth, 14 * (lines-1) + 10)
 end
 
 function widget:Initialize()
-	Chili = WG.Chili
-	local screen = Chili.Screen0
-	--control0 = Chili.Control:New{parent=screen,right=500,y=20,height=30,width=300,padding={0,0,0,0}}
-	window0 = Chili.Window:New{parent=screen,right=500,y=20,minHeight=20,height=30,width=300,padding={0,0,0,0}}
-	editbox0 = Chili.EditBox:New{parent=window0,right=0,x=0,y=0,right=0,bottom=0,text='  --Takes over Enter/Return when entering text--',
-	OnMouseDown = {function(obj) obj.text = '' end}}
-	conOut = Chili.Window:New{parent=screen,padding = {5,0,2,0},minHeight = 15,right = 450,y = 50,width = 300}
 	
-	local buffer = Spring.GetConsoleBuffer(200)
+	Chili = WG.Chili
+	
+	local screen = Chili.Screen0
+	
+	window0 = Chili.Window:New{
+		parent   = screen,
+		right    = 500,
+		y        = 20,
+		minHeight= 20,
+		height   = 30,
+		width    = 300,
+		padding  = {0,0,0,0}
+	}
+	
+	editbox0 = Chili.EditBox:New{
+		parent      = window0,
+		right       = 0,
+		x           = 0,
+		y           = 0,
+		right       = 0,
+		bottom      = 0,
+		text        = '  --Takes over Enter/Return when entering text--',
+		OnMouseDown =  {function(obj) obj.text = '' end}
+	}
+	
+	msgWindow = Chili.Window:New{
+		parent    = screen,
+		padding   = {5,5,2,5},
+		minHeight = 15,
+		right     = 450,
+		y         = 50,
+		width     = msgWidth
+	}
+	textBox = Chili.TextBox:New{
+		parent = msgWindow,
+		width  = msgWidth,
+		text   = ''
+	}
+	local buffer = getConsoleBuffer(200)
+	
 	for i=1,#buffer do
 		widget:AddConsoleLine(buffer[i].text,buffer[i].priority)
 	end
 	
-	Spring.SendCommands('console 0')
-	Spring.SendCommands('inputtextgeo '..(window0.x/Chili.Screen0.width-0.035)..' '
-	..(1 - (window0.y + window0.height) / Chili.Screen0.height + 0.003)..' 0.02 '..(window0.width / Chili.Screen0.width))
+	-- Disable engine console
+	sendCommands('console 0')
+	
+	-- Move input to line up with new console
+	sendCommands('inputtextgeo '
+		-- ..(window0.x/Chili.Screen0.width-0.035)..' '
+		..(window0.x/Chili.Screen0.width)..' '
+		-- ..(1 - (window0.y + window0.height) / Chili.Screen0.height + 0.003)
+		..(1 - (window0.y + window0.height) / Chili.Screen0.height)
+		..' 0.1 '
+		..(window0.width / Chili.Screen0.width) )
+		
 end
 
-
-function widget:GameFrame(n)
-	local numClearMsg=0
-	for i=1, numShowMsg do
-		local msg = messages[#messages-numShowMsg+i]
-		if msg.t + 300 < n then
-			numClearMsg = numClearMsg+1
-		end
-	end
-	numShowMsg= math.max(0,numShowMsg-numClearMsg)
-	if numClearMsg>0 or numShowMsg>0 then
+-- Adds dissappearing text
+function widget:DrawScreen()
+	timer = getTimer()
+	if diffTimers(timer, oldTimer) > msgTime and curMsgNum > 0 then
+		curMsgNum = curMsgNum - 1
+		oldTimer = timer
 		updateMsgWindow()
 	end
 end
 
 function widget:AddConsoleLine(text,priority)
-	local msgtime=Spring.GetGameFrame()
 	messages[#messages+1] = {}
-	messages[#messages] = {text=text, priority=priority, t=msgtime}
+	messages[#messages] = {text=text, priority=priority}
+	if curMsgNum < maxMsgNum then curMsgNum = curMsgNum+1 end
+	oldTimer = getTimer()
 	updateMsgWindow()
-	if numShowMsg < 6 then numShowMsg = numShowMsg+1 end
 end
 
 function widget:KeyPress(key)
 	if key==13 then
 		if editbox0.state.focused then
 			if string.find(editbox0.text,'/')==1 then
-				Spring.SendCommands(string.sub(editbox0.text,2))
+				sendCommands(string.sub(editbox0.text,2))
 			else
-				Spring.SendCommands('Say '..editbox0.text)
+				sendCommands('Say '..editbox0.text)
 			end
-			-- else
-			-- editbox0.state.focused = true
-			-- editbox0.text=''
-			-- editbox0:Update()
 			return true
 		end
 		editbox0.text = ''
@@ -100,6 +150,6 @@ function widget:KeyPress(key)
 end
 
 function widget:Shutdown()
-	Spring.SendCommands({'console 1', 'inputtextgeo default'})
-	Spring.SetConfigString('InputTextGeo', '0.26 0.73 0.02 0.028')
+	sendCommands({'console 1', 'inputtextgeo default'})
+	setConfigString('InputTextGeo', '0.26 0.73 0.02 0.028')
 end

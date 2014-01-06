bbcc28b581908cc9d051e6e129981c1eb2f79fc3
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-01-06 20:58:44 +0000

    BAR: add dbg_unittexture_preloader.lua to preload normals for customunitshaders

---
+++ branches/BAR/luaui/widgets/dbg_unittexture_preloader.lua
@@ -0,0 +1,39 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+function widget:GetInfo()
+  return {
+    name      = "Unittexture Preloader",
+    desc      = "Preloads unittextures, so no lag on start (first built unit)",
+    author    = "beherith",
+    date      = "2014 jan",
+    license   = "GPLv2",
+    layer     = 1000,
+    enabled   = true,  --  loaded by default?
+  }
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local files = {
+	--"unittextures/Arm_color.dds", --preloading these (they are used by S30) seems to have no effect
+	--"unittextures/Arm_other.dds",
+	"unittextures/Core_normals.dds", -- this does reduce the stall when first unit using this is constructed, but also it should only preload if luaunitshader are enabled? (else its a waste of time and gpu ram)
+	"unittextures/Arm_normals.dds",
+}
+
+
+
+
+function widget:DrawGenesis()
+	for i,file in ipairs(files) do
+		gl.Texture(7,file)
+		gl.Texture(7,false)
+		Spring.Echo('Preloaded',file)
+	end
+	widgetHandler:RemoveWidget()
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------

211774c53ac410a8417cd8b5000627b203eb0bf3
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-04-14 22:08:08 +0000

    BAR: added arm emp spider

--- branches/BAR/units/armspid.lua
+++ branches/BAR/units/armspid.lua
@@ -23,11 +23,15 @@ return {
 		movementclass = "TKBOT2",
 		name = "Spider",
 		nochasecategory = "ALL",
-		objectname = "ARMSPID",
+		objectname = "ARMSPID.s3o",
 		seismicsignature = 0,
 		selfdestructas = "BIG_UNIT",
 		sightdistance = 550,
 		turnrate = 1122,
+		customparams = {
+			normalmaps = "yes",
+			normaltex = "unittextures/Arm_normals.dds",
+		},
 		featuredefs = {
 			dead = {
 				blocking = false,

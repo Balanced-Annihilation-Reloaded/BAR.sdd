220f13abf4ab04552d5090dd08d267e00b087d5d
Floris <Floris@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-05-19 15:19:23 +0000

    Comblast/dgun range - line thicker, overall lower visibility, Fades depending on camera distance Added line description on low camera distance Added line glow option with: /comranges_glow

--- branches/BAR/luaui/widgets/gui_comblast_dgun_range.lua
+++ branches/BAR/luaui/widgets/gui_comblast_dgun_range.lua
@@ -10,7 +10,14 @@ function widget:GetInfo()
     }
 end
 
--- locals --
+--------------------------------------------------------------------------------
+-- Console commands
+--------------------------------------------------------------------------------
+
+--/comranges_glow			-- toggles a faint glow on the line
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 local pairs					= pairs
 
@@ -23,21 +30,34 @@ local spGetPlayerInfo		= Spring.GetPlayerInfo
 local spGetGroundHeight		= Spring.GetGroundHeight
 local spIsSphereInView		= Spring.IsSphereInView
 local spValidUnitID			= Spring.ValidUnitID
-
-
+local spGetCameraPosition	= Spring.GetCameraPosition
 local glDepthTest 			= gl.DepthTest
 local glDrawGroundCircle 	= gl.DrawGroundCircle
+local glLineWidth 			= gl.LineWidth
 local glColor				= gl.Color
+local glTranslate			= gl.Translate
+local glRotate				= gl.Rotate
+local glText				= gl.Text
 local GL_ALWAYS				= GL.ALWAYS
-
-local circleDivs = 32
-local blastRadius = 360 -- com explosion
-local dgunRange = WeaponDefNames["armcom_arm_disintegrator"].range + 2*WeaponDefNames["armcom_arm_disintegrator"].damageAreaOfEffect
 
 local comCenters = {}
 local drawList
 local amSpec = false
 local inSpecFullView = false
+
+--------------------------------------------------------------------------------
+-- OPTIONS
+--------------------------------------------------------------------------------
+
+local circleDivs			= 64		-- circle detail, when fading out it will lower this aswell (so dont go too low)
+local blastRadius			= 360		-- com explosion
+local dgunRange				= WeaponDefNames["armcom_arm_disintegrator"].range + 2*WeaponDefNames["armcom_arm_disintegrator"].damageAreaOfEffect
+local showTitles			= true		-- shows title text around the circle-line
+local showTitleDistance		= 750
+local showLineGlow 			= true		-- a ticker but faint 2nd line will be drawn underneath		
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
 
 
 -- track coms --
@@ -178,14 +198,159 @@ end
 
 -- draw circles
 function widget:DrawWorldPreUnit()
+	local camX, camY, camZ = spGetCameraPosition()
 	glDepthTest(GL_ALWAYS)
 	for _,center in pairs(comCenters) do
 		if center[4] then
-			glColor(1, 0.8, 0, .6)
-			glDrawGroundCircle(center[1], center[2], center[3], dgunRange, circleDivs)
-			glColor(1, 0, 0, .35)
-			glDrawGroundCircle(center[1], center[2], center[3], blastRadius, circleDivs)
+		
+			local xDifference = camX - center[1]
+			local yDifference = camY - center[2]
+			local zDifference = camZ - center[3]
+			local camDistance = math.sqrt(xDifference*xDifference + yDifference*yDifference + zDifference*zDifference)
+			
+			local lineWidthMinus = (camDistance/2000)
+			if lineWidthMinus > 2 then
+				lineWidthMinus = 2
+			end
+			local lineOpacityMultiplier = (1200/camDistance)
+			if lineOpacityMultiplier > 1 then
+				lineOpacityMultiplier = 1
+			end
+			if lineOpacityMultiplier > 0.18 then
+				if showTitles and camDistance < showTitleDistance then
+					-- DGUN titles
+					local lineDistance	= 5
+					local fontSize		= 10
+					local radius		= dgunRange
+					local text = "DGUN"
+					
+					glColor(1, 0.8, 0, .3*lineOpacityMultiplier)
+					glTranslate(center[1], 0, center[3])
+					
+					local mapHeight = spGetGroundHeight(center[1],center[3]-radius)
+					glTranslate(0,mapHeight,-radius)
+					glRotate(90,-1,0,0)
+					glText(text, 0, lineDistance, fontSize, "cn")
+					glRotate(90,1,0,0)
+					glTranslate(0,-mapHeight,radius)
+					
+					mapHeight = spGetGroundHeight(center[1],center[3]+radius)
+					glTranslate(0,mapHeight,radius)
+					glRotate(90,-1,0,0)
+					glRotate(180,0,0,1)
+					glText(text, 0, lineDistance, fontSize, "cn")
+					glRotate(180,0,0,-1)
+					glRotate(90,1,0,0)
+					glTranslate(0,-mapHeight,-radius)
+					
+					mapHeight = spGetGroundHeight(center[1]-radius,center[3])
+					glTranslate(-radius,mapHeight,0)
+					glRotate(90,-1,0,0)
+					glRotate(90,0,0,1)
+					glText(text, 0, lineDistance, fontSize, "cn")
+					glRotate(90,0,0,-1)
+					glRotate(90,1,0,0)
+					glTranslate(radius,-mapHeight,0)
+					
+					mapHeight = spGetGroundHeight(center[1]+radius,center[3])
+					glTranslate(radius,mapHeight,0)
+					glRotate(90,-1,0,0)
+					glRotate(270,0,0,1)
+					glText(text, 0, lineDistance, fontSize, "cn")
+					glRotate(270,0,0,-1)
+					glRotate(90,1,0,0)
+					glTranslate(-radius,-mapHeight,0)
+					
+					glTranslate(-center[1], 0, -center[3])
+					
+					
+					-- Com explosion range titles
+					lineDistance	= 5
+					fontSize		= 10
+					radius			= blastRadius
+					text			= "BLAST"
+					
+					glColor(1, 0, 0, .37*lineOpacityMultiplier)
+					glTranslate(center[1], 0, center[3])
+					
+					local mapHeight = spGetGroundHeight(center[1],center[3]-radius)
+					glTranslate(0,mapHeight,-radius)
+					glRotate(90,-1,0,0)
+					glText(text, 0, lineDistance, fontSize, "cn")
+					glRotate(90,1,0,0)
+					glTranslate(0,-mapHeight,radius)
+					
+					mapHeight = spGetGroundHeight(center[1],center[3]+radius)
+					glTranslate(0,mapHeight,radius)
+					glRotate(90,-1,0,0)
+					glRotate(180,0,0,1)
+					glText(text, 0, lineDistance, fontSize, "cn")
+					glRotate(180,0,0,-1)
+					glRotate(90,1,0,0)
+					glTranslate(0,-mapHeight,-radius)
+					
+					mapHeight = spGetGroundHeight(center[1]-radius,center[3])
+					glTranslate(-radius,mapHeight,0)
+					glRotate(90,-1,0,0)
+					glRotate(90,0,0,1)
+					glText(text, 0, lineDistance, fontSize, "cn")
+					glRotate(90,0,0,-1)
+					glRotate(90,1,0,0)
+					glTranslate(radius,-mapHeight,0)
+					
+					mapHeight = spGetGroundHeight(center[1]+radius,center[3])
+					glTranslate(radius,mapHeight,0)
+					glRotate(90,-1,0,0)
+					glRotate(270,0,0,1)
+					glText(text, 0, lineDistance, fontSize, "cn")
+					glRotate(270,0,0,-1)
+					glRotate(90,1,0,0)
+					glTranslate(-radius,-mapHeight,0)
+					
+					glTranslate(-center[1], 0, -center[3])
+				end
+				
+				-- draw lines
+				if showLineGlow then
+					glLineWidth(10-lineWidthMinus)
+					glColor(1, 0.8, 0, .04*lineOpacityMultiplier)
+					glDrawGroundCircle(center[1], center[2], center[3], dgunRange, circleDivs*lineOpacityMultiplier)
+					
+					glLineWidth(10-lineWidthMinus)
+					glColor(1, 0, 0, .055*lineOpacityMultiplier)
+					glDrawGroundCircle(center[1], center[2], center[3], blastRadius, (circleDivs*1.2)*lineOpacityMultiplier)
+				end
+				glLineWidth(3-lineWidthMinus)
+				glColor(1, 0.8, 0, .36*lineOpacityMultiplier)
+				glDrawGroundCircle(center[1], center[2], center[3], dgunRange, circleDivs*lineOpacityMultiplier)
+				
+				glLineWidth(3.3-lineWidthMinus)
+				glColor(1, 0, 0, .48*lineOpacityMultiplier)
+				glDrawGroundCircle(center[1], center[2], center[3], blastRadius, (circleDivs*1.2)*lineOpacityMultiplier)
+			end
 		end
 	end
+	glLineWidth(1)
 	glDepthTest(false)
+end
+
+function widget:GetConfigData(data)
+    savedTable = {}
+    savedTable.showLineGlow = showLineGlow
+    return savedTable
+end
+
+function widget:SetConfigData(data)
+    if data.showLineGlow ~= nil 	then  autoFade	= data.showLineGlow end
+end
+
+function widget:TextCommand(command)
+    if (string.find(command, "comranges_glow") == 1  and  string.len(command) == 14) then 
+		showLineGlow = not showLineGlow
+		if autoFade then
+			Spring.Echo("Pause screen:  Glow on")
+		else
+			Spring.Echo("Pause screen:  Glow off")
+		end
+	end
 end

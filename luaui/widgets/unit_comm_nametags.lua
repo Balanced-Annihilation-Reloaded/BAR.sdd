de44f2456a20a52a022b640375e7a763e14ab59c
Floris <11016921+Ruwetuin@users.noreply.github.com>
2014-05-23 20:46:37 +0000

    Comm Nametags - changed font style (black/white outlines) and added some thickness font scales with camera distance, player info only fades in at low camera height
    added console cmd's:
    /comnames_thick
    /comnames_fade
    /comnames_info

--- luaui/widgets/unit_comm_nametags.lua
+++ luaui/widgets/unit_comm_nametags.lua
@@ -1,4 +1,4 @@
-local versionNumber = "1.80"
+local versionNumber = "1.81"
 
 function widget:GetInfo()
   return {
@@ -16,24 +16,40 @@ end
 -- Console commands
 --------------------------------------------------------------------------------
 
+--/comnames_thick
+--/comnames_fade
+--/comnames_info
+
 --/comnames_rank
 --/comnames_trueskill
 --/comnames_flag
-
 
 --------------------------------------------------------------------------------
 -- config
 --------------------------------------------------------------------------------
 
-local showStickyTags	= false --comms literally wear name tags
-local heightOffset		= 28
-local scale				= 0.75
-local xOffset			= 0
-local yOffset			= 0
-local fontSize			= 6
-local showRank			= true
-local showTrueskill		= true
-local showCountry		= true
+local heightOffset			= 28
+local infoScale				= 0.75
+local xOffset				= 0
+local yOffset				= 0
+
+local fontSize				= 13.5
+local scaleFontSize			= true
+local scaleFontAmount		= 140
+
+local useThickLeterring		= true
+
+local showInfo 				= true
+local showRank				= true		-- needs showInfo=true
+local showTrueskill			= true		-- needs showInfo=true
+local showCountry			= true		-- needs showInfo=true
+
+local fadeNames				= false
+local fadeStartHeight		= 3200
+local fadeEndHeight			= 5200
+
+local fadeIconStartHeight	= 1250		
+local fadeIconEndHeight		= 1660		--needs to be smaller than fadeEndHeight
 
 --------------------------------------------------------------------------------
 -- speed-ups
@@ -48,18 +64,17 @@ local GetVisibleUnits     = Spring.GetVisibleUnits
 local GetUnitDefID        = Spring.GetUnitDefID
 local GetAllUnits         = Spring.GetAllUnits
 local GetUnitHeading      = Spring.GetUnitHeading
-
-local iconsize   = 10
-local iconhsize  = iconsize * 0.5
-
+local GetCameraPosition   = Spring.GetCameraPosition
+local GetUnitPosition     = Spring.GetUnitPosition
 
 local glColor             = gl.Color
---local glText              = gl.Text
+local glText              = gl.Text
 local glPushMatrix        = gl.PushMatrix
 local glPopMatrix         = gl.PopMatrix
 local glTranslate         = gl.Translate
 local glBillboard         = gl.Billboard
 local glDrawFuncAtUnit    = gl.DrawFuncAtUnit
+local glBlending          = gl.Blending
 
 local glDepthTest      = gl.DepthTest
 local glAlphaTest      = gl.AlphaTest
@@ -67,15 +82,15 @@ local glTexture        = gl.Texture
 local glTexRect        = gl.TexRect
 local glRotate         = gl.Rotate
 local GL_GREATER       = GL.GREATER
+local GL_SRC_ALPHA				= GL.SRC_ALPHA	
+local GL_ONE_MINUS_SRC_ALPHA	= GL.ONE_MINUS_SRC_ALPHA
 local glUnitMultMatrix = gl.UnitMultMatrix
 local glUnitPieceMultMatrix = gl.UnitPieceMultMatrix
 local glScale          = gl.Scale
 
 
-local overheadFont	= "LuaUI/Fonts/FreeSansBold_14"
-local stickyFont	= "LuaUI/Fonts/FreeSansBold_14"
-local fhDraw		= fontHandler.Draw
-
+local usedFontSize	 = fontSize
+local font = gl.LoadFont("Fonts/FreeSansBold.otf",50, 6, 15)
 
 --rank pics
 local rankImages = {}
@@ -111,6 +126,7 @@ function GetSkill(playerID)
 	local customtable = select(10,GetPlayerInfo(playerID)) -- player custom table
 	local tsMu = customtable.skill
 	local tsSigma = customtable.skilluncertainty
+	local color = {1,1,1}
 	local tskill = ""
 	local tskillValue = ""
 	if tsMu then
@@ -119,6 +135,7 @@ function GetSkill(playerID)
 		tskillValue = tskill
 		if string.find(tsMu, ")") then
 			tskill = "\255"..string.char(190)..string.char(140)..string.char(140) .. tskill -- ')' means inferred from lobby rank
+			color = {190/255, 140/255, 140/255}
 		else
 		
 			-- show privacy mode
@@ -141,18 +158,21 @@ function GetSkill(playerID)
 						tsRed, tsGreen, tsBlue = 250, 250, 250
 				end
 				tskill = priv .. "\255"..string.char(tsRed)..string.char(tsGreen)..string.char(tsBlue) .. tskill
+				color = {tsRed/255, tsGreen/255, tsBlue/255}
 			else
 				tskill = priv .. "\255"..string.char(195)..string.char(195)..string.char(195) .. tskill --should never happen
+				color = {195/255, 195/255, 195/255}
 			end
 			if priv ~= "" then
 				tskillValue = "*"..tskillValue
 			end
 		end
 	else
-		tskill = "\255"..string.char(160)..string.char(160)..string.char(160) .. "?"
 		tskillValue = "?"
+		tskill = "\255"..string.char(160)..string.char(160)..string.char(160) .. "?"
+		color = {160/255, 160/255, 160/255}
 	end
-	return {tskillValue, tskill}
+	return {tskill, tskillValue, color}
 end
 
 
@@ -175,56 +195,64 @@ local function GetCommAttributes(unitID, unitDefID)
 end
 
 local function DrawCommName(unitID, attributes)
+       
+  local iconHeight = 17*(infoScale+((1-infoScale)/2))
+  
   glTranslate(0, attributes[3], 0 )
   glBillboard()
-  glColor(attributes[2])
-  --glText(attributes[1], xOffset, yOffset, fontSize, "cn")
-  fontHandler.UseFont(overheadFont)
-  fontHandler.DrawCentered(attributes[1], xOffset,yOffset)
-  
-  
-  glTranslate(0, 17*(scale+((1-scale)/2)), 0 )
-  glScale(scale,scale,scale)
-  if showCountry and attributes[6] and attributes[6] ~= '' then
-	glColor(1,1,1,0.9)
-    glTexture("LuaUI/Images/flags/"..string.lower(attributes[6])..".png")
-    glTexRect(-22.5, 0.5, -10.5, 8.75)
-    glTexture(false)
-  end
-  if showTrueskill and attributes[7] and attributes[7][1] and attributes[7][1] ~= "?" then
-	glColor(0,0,0,0.6)
-    gl.Text(tostring(attributes[7][1]),8.2,0.8,10.5,"n")
-	glColor(1,1,1,0.9)
-    gl.Text(tostring(attributes[7][2]),8.2,1.3,10.5,"n")
-  end
-  if showRank and rankImages[tonumber(attributes[5])] then
-    glColor(1,1,1,0.9)
-    glTexture(rankImages[tonumber(attributes[5])])
-    glTexRect(-13/2,2.5, 13/2, 2.5+13)
-    glTexture(false)
+   
+  font:Begin()
+  if useThickLeterring then
+    if (attributes[2][1] + attributes[2][2] + attributes[2][3]*0.5) < 1 then
+      font:SetTextColor({1,1,1,0.9*attributes[2][4]*attributes[2][4]*attributes[2][4]*attributes[2][4]})
+      font:SetOutlineColor(({1,1,1,0.9*attributes[2][4]*attributes[2][4]*attributes[2][4]*attributes[2][4]}))
+    else
+      font:SetTextColor({0,0,0,0.9*attributes[2][4]*attributes[2][4]*attributes[2][4]*attributes[2][4]})
+      font:SetOutlineColor(({0,0,0,0.9*attributes[2][4]*attributes[2][4]*attributes[2][4]*attributes[2][4]}))
+    end
+    font:Print(attributes[1], xOffset-0.3, yOffset-0.66, usedFontSize, "con")
+    font:Print(attributes[1], xOffset+0.3, yOffset-0.66, usedFontSize, "con")
   end
   
-  glScale(1,1,1)
-  glColor(1,1,1,1)
-end
-
-local function DrawNameTag(rotation)
-  glRotate(rotation,0,1,0)
-  glTranslate(8, 35, 7)
+  font:SetTextColor(attributes[2])
   
-  glColor(1,1,1,1)
-  glTexRect(-iconhsize, 0, iconhsize, iconsize)
+  -- not acurate (enough)   but...   font:SetAutoOutlineColor(true)   doesnt seem to work
+  if (attributes[2][1] + attributes[2][2] + attributes[2][3]*0.5) < 1 then
+    font:SetOutlineColor(({1,1,1,0.87*attributes[2][4]*attributes[2][4]*attributes[2][4]}))
+  else
+    font:SetOutlineColor(({0,0,0,0.87*attributes[2][4]*attributes[2][4]*attributes[2][4]}))
+  end
+  font:Print(attributes[1], xOffset, yOffset, usedFontSize, "con")
+  
+  if showInfo and showTrueskill and attributes[7] and attributes[7][1] and attributes[7][1] ~= "?" then
+    font:SetTextColor({0,0,0,0.55 * attributes[8]})
+    font:Print(attributes[7][2], 13.5*infoScale, (iconHeight+1.5)*infoScale, 8*infoScale, "cn")
+    
+    font:SetTextColor({attributes[7][3][1],attributes[7][3][2],attributes[7][3][3],0.95 * attributes[8]})
+    font:Print(attributes[7][2], 13.5*infoScale, (iconHeight+1.9)*infoScale, 8*infoScale, "cn")
+  end
+  font:End()
+  
+  if showInfo then
+    glScale(infoScale,infoScale,infoScale)
+    
+    if showCountry and attributes[6] and attributes[6] ~= '' then
+      glColor(1,1,1,0.9 * attributes[8])
+      glTexture("LuaUI/Images/flags/"..string.lower(attributes[6])..".png")
+      glTexRect(-22.5, iconHeight+0.5, -10.5, iconHeight+8.75)
+      glTexture(false)
+    end
+    if showRank and rankImages[tonumber(attributes[5])] then
+      glColor(1,1,1,0.9 * attributes[8])
+      glTexture(rankImages[tonumber(attributes[5])])
+      glTexRect(-13/2, iconHeight+2.5, 13/2, iconHeight+2.5+13)
+      glTexture(false)
+    end
+    glScale(1,1,1)
+    glColor(1,1,1,1)
+  end
 end
 
-local function DrawCommName2(unitID, attributes, rotation)
-  glRotate(rotation,0,1,0)
-  glTranslate(8, 40, 7)
-
-  glColor(attributes[2])
-  --glText(attributes[1], xOffset, yOffset, 1, "cn")
-
-  glColor(1,1,1,1)
-end
 
 --------------------------------------------------------------------------------
 -- callins
@@ -255,59 +283,49 @@ end
 
 
 function widget:DrawWorld()
-
   --if Spring.IsGUIHidden() then return end
 
   glDepthTest(true)
   glAlphaTest(GL_GREATER, 0)
-  if (showStickyTags) then
-    glTexture('LuaUI/Images/hellomynameis.png')
-    for unitID, attributes in pairs(comms) do
-	  if (attributes[4]) then
-	      glPushMatrix()
-	      glUnitMultMatrix(unitID)
-	      glUnitPieceMultMatrix(unitID, attributes[4])
-	      glRotate(0,0,1,0)
-	      glTranslate(8, 0, 7)
-	      glColor(1,1,1,1)
-	      glTexRect(-iconhsize, 0, iconhsize, iconsize)
-	      glPopMatrix()
-		end
-    end
-    for unitID, attributes in pairs(comms) do
-	  if (attributes[4]) then
-	      glPushMatrix()
-	      glUnitMultMatrix(unitID)
-	      glUnitPieceMultMatrix(unitID, attributes[4])
-	      glRotate(0,0,1,0)
-	      glTranslate(8, 0, 7)
-	      glColor(attributes[2])
-	 
-	      glPushMatrix()
-	      glScale(0.03, 0.03, 0.03)
-	      glTranslate (0,120,5)
-	      fontHandler.UseFont(stickyFont)
-	      fontHandler.DrawCentered(attributes[1], 0,0)
-	      glPopMatrix()
-	 
-	      glPopMatrix()
-	  end
-    end
-
-  end
+  glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
       
+  local camX, camY, camZ = GetCameraPosition()
   for unitID, attributes in pairs(comms) do
     local heading = GetUnitHeading(unitID)
     if (not heading) then
       return
     end
-    local rot = (heading / 32768) * 180
-    glDrawFuncAtUnit(unitID, false, DrawCommName, unitID, attributes)
-    if (showStickyTags) then
-      glDrawFuncAtUnit(unitID, false, DrawCommName2, unitID, attributes, rot)
+    
+    -- calc opacity
+    local x,y,z = GetUnitPosition(unitID)
+    local xDifference = camX - x
+    local yDifference = camY - y
+    local zDifference = camZ - z
+    local camDistance = math.sqrt(xDifference*xDifference + yDifference*yDifference + zDifference*zDifference)
+    
+    if scaleFontSize then
+      usedFontSize = (fontSize*0.5) + (camDistance/scaleFontAmount)
+    end
+    local opacityMultiplier = 1
+    if fadeNames then
+		opacityMultiplier = 1 - (camDistance-fadeStartHeight) / (fadeEndHeight-fadeStartHeight)
+		if opacityMultiplier > 1 then
+			opacityMultiplier = 1
+		end
+    end
+    local iconOpacityMultiplier = 1 - (camDistance-fadeIconStartHeight) / (fadeIconEndHeight-fadeIconStartHeight)
+    if iconOpacityMultiplier > 1 then
+    	iconOpacityMultiplier = 1
+    end
+    
+    if opacityMultiplier > 0 then
+      attributes[2][4] = opacityMultiplier
+      attributes[8] = iconOpacityMultiplier
+      local rot = (heading / 32768) * 180
+      glDrawFuncAtUnit(unitID, false, DrawCommName, unitID, attributes)
     end
   end
-
+  
   glAlphaTest(false)
   glColor(1,1,1,1)
   glTexture(false)
@@ -339,16 +357,22 @@ end
 
 function widget:GetConfigData(data)
     savedTable = {}
-    savedTable.showRank			= showRank
-    savedTable.showTrueskill	= showTrueskill
-    savedTable.showCountry		= showCountry
+    savedTable.showRank				= showRank
+    savedTable.showTrueskill		= showTrueskill
+    savedTable.showCountry			= showCountry
+    savedTable.fadeNames			= fadeNames
+    savedTable.useThickLeterring	= useThickLeterring
+    savedTable.showInfo				= showInfo
     return savedTable
 end
 
 function widget:SetConfigData(data)
-    if data.showRank ~= nil 		then  showRank		= data.showRank end
-    if data.showTrueskill ~= nil 	then  showTrueskill	= data.showTrueskill end
-    if data.showCountry ~= nil	 	then  showCountry	= data.showCountry end
+    if data.showRank ~= nil 			then  showRank				= data.showRank end
+    if data.showTrueskill ~= nil 		then  showTrueskill			= data.showTrueskill end
+    if data.showCountry ~= nil	 		then  showCountry			= data.showCountry end
+    if data.fadeNames ~= nil	 		then  fadeNames				= data.fadeNames end
+    if data.useThickLeterring ~= nil	then  useThickLeterring		= data.useThickLeterring end
+    if data.showInfo ~= nil				then  showInfo				= data.showInfo end
 end
 
 function widget:TextCommand(command)
@@ -361,4 +385,14 @@ function widget:TextCommand(command)
     if (string.find(command, "comnames_flag") == 1  and  string.len(command) == 13) then 
 		showCountry = not showCountry
 	end
+    if (string.find(command, "comnames_info") == 1  and  string.len(command) == 13) then 
+		showInfo = not showInfo
+	end
+    if (string.find(command, "comnames_fade") == 1  and  string.len(command) == 13) then 
+		fadeNames = not fadeNames
+	end
+    if (string.find(command, "comnames_thick") == 1  and  string.len(command) == 14) then 
+		useThickLeterring = not useThickLeterring
+	end
+	
 end

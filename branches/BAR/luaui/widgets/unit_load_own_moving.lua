c6becc3eb5cd64f2a162540a61915b94f2c9ab47
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-06-20 16:30:30 +0000

    match filenames (ba<->bar)

--- branches/BAR/luaui/widgets/unit_load_own_moving.lua
+++ branches/BAR/luaui/widgets/unit_load_own_moving.lua
@@ -1,8 +1,8 @@
 
 function widget:GetInfo()
 	return {
-		name      = "Transport Moving Units",
-		desc      = "Enables you to load units while they are moving",
+		name      = "Load Own Moving",
+		desc      = "Enables loading of your own units when they're moving",
 		author    = "Niobium",
 		version   = "1.0",
 		date      = "June 18, 2010",

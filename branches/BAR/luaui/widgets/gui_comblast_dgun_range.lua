9845f1a13f433790c18096f05221f22427e1bb05
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-07-29 23:08:05 +0000

    comblast_dgun_range only draws when an enemy unit is within 2x(com exposion range) bar version matches visuals of all other range circles

--- branches/BAR/luaui/widgets/gui_comblast_dgun_range.lua
+++ branches/BAR/luaui/widgets/gui_comblast_dgun_range.lua
@@ -2,7 +2,7 @@ function widget:GetInfo()
     return {
         name      = "Comblast & Dgun Range",
         desc      = "Shows the range of commander death explosion and dgun ranges",
-        author    = "Bluestone, based on similar widgets by vbs, tfc, decay  (made fancy by Floris)",
+        author    = "Bluestone, based on similar widgets by vbs, tfc, decay",
         date      = "11/2013",
         license   = "GPL v3 or later",
         layer     = 0,
@@ -10,17 +10,12 @@ function widget:GetInfo()
     }
 end
 
---------------------------------------------------------------------------------
--- Console commands
---------------------------------------------------------------------------------
-
---/comranges_glow				-- toggles a faint glow on the line
---/comranges_nearbyenemy		-- toggles hiding of ranges when enemy is nearby
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
+-- locals --
 
 local pairs					= pairs
+local sqrt                  = math.sqrt
+local min                   = math.min
+local max                   = math.max
 
 local spGetUnitPosition     = Spring.GetUnitPosition
 local spGetUnitDefID 		= Spring.GetUnitDefID
@@ -31,52 +26,31 @@ local spGetPlayerInfo		= Spring.GetPlayerInfo
 local spGetGroundHeight		= Spring.GetGroundHeight
 local spIsSphereInView		= Spring.IsSphereInView
 local spValidUnitID			= Spring.ValidUnitID
-local spGetCameraPosition	= Spring.GetCameraPosition
-local spGetUnitNearestEnemy	= Spring.GetUnitNearestEnemy
+local spIsGUIHidden         = Spring.IsGUIHidden
+local spGetUnitNearestEnemy = Spring.GetUnitNearestEnemy
+local spGetUnitPosition     = Spring.GetUnitPosition
+
 
 local glDepthTest 			= gl.DepthTest
 local glDrawGroundCircle 	= gl.DrawGroundCircle
-local glLineWidth 			= gl.LineWidth
 local glColor				= gl.Color
-local glTranslate			= gl.Translate
-local glRotate				= gl.Rotate
-local glText				= gl.Text
-local glBlending			= gl.Blending
+local GL_ALWAYS				= GL.ALWAYS
 
-local GL_ALWAYS					= GL.ALWAYS
-local GL_SRC_ALPHA				= GL.SRC_ALPHA
-local GL_ONE_MINUS_SRC_ALPHA	= GL.ONE_MINUS_SRC_ALPHA
+local circleDivs = 32
+local blastRadius = 360 -- com explosion
+local dgunRange = WeaponDefNames["armcom_arm_disintegrator"].range + 2*WeaponDefNames["armcom_arm_disintegrator"].damageAreaOfEffect
 
 local comCenters = {}
 local drawList
 local amSpec = false
 local inSpecFullView = false
-local dgunRange	= WeaponDefNames["armcom_arm_disintegrator"].range + 2*WeaponDefNames["armcom_arm_disintegrator"].damageAreaOfEffect
-
---------------------------------------------------------------------------------
--- OPTIONS
---------------------------------------------------------------------------------
-
-local opacityMultiplier		= 1
-local fadeMultiplier		= 1.2			-- lower value: fades out sooner
-local circleDivs			= 64		-- circle detail, when fading out it will lower this aswell (minimum always will be 40 anyway)
-local blastRadius			= 360		-- com explosion
-local showTitles			= true		-- shows title text around the circle-line
-local showTitleDistance		= 750
-local showLineGlow 			= true		-- a ticker but faint 2nd line will be drawn underneath	
-local hideOnDistantEnemy	= true
-local showOnEnemyDistance	= 650
-local fadeInDistance		= 350
-local smoothoutTime			= 2.2		-- time to smoothout sudden changes (value = time between max and zero opacity)
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
 
 
 -- track coms --
 
 function widget:Initialize()
+    widgetHandler:RegisterGlobal('SetOpacity_Comblast_DGun_Range', SetOpacity)
+
     checkComs()
 	checkSpecView()
     return true
@@ -85,7 +59,7 @@ end
 function addCom(unitID)
 	if not spValidUnitID(unitID) then return end --because units can be created AND destroyed on the same frame, in which case luaui thinks they are destroyed before they are created
 	local x,y,z = Spring.GetUnitPosition(unitID)
-	comCenters[unitID] = {x,y,z}
+	comCenters[unitID] = {x,y,z,false,0}
 end
 
 function removeCom(unitID)
@@ -146,6 +120,7 @@ function widget:PlayerChanged(playerID)
 end
 
 function widget:GameOver()
+    widgetHandler:DeregisterGlobal('SetOpacity_Comblast_DGun_Range', SetOpacity)
 	widgetHandler:RemoveWidget()
 end
 
@@ -169,7 +144,7 @@ function checkComs()
         for _, unitID in ipairs(visibleUnits) do
             local unitDefID = spGetUnitDefID(unitID)
             if unitDefID and UnitDefs[unitDefID].customParams.iscommander == "1" then
-				addCom(unitID)
+                addCom(unitID)
             end
         end
     end
@@ -186,16 +161,25 @@ function widget:GameFrame(n)
 		checkComs()
 		inSpecFullView = specFullView
     end
-    
+
 	-- check com movement
 	for unitID in pairs(comCenters) do
 		local x,y,z = spGetUnitPosition(unitID)
 		if x then
-			local osClock = os.clock()
 			local yg = spGetGroundHeight(x,z) 
 			local draw = true
-			local opacityMultiplier = 1
-			oldOpacityMultiplier = opacityMultiplier
+            local opacity 
+            local wantedOpacity
+            -- check if there is an enemy unit nearby
+            local enemyUnitID = spGetUnitNearestEnemy(unitID,2*blastRadius,false)
+			if enemyUnitID then
+				local ex,ey,ez = spGetUnitPosition(enemyUnitID)
+				local distance = sqrt((x-ex)^2 + (y-ey)^2 + (z-ez)^2)
+                wantedOpacity = 0.8 - 0.8*max(distance-blastRadius,0)/blastRadius
+            else
+                wantedOpacity = 0
+            end
+            opacity = comCenters[unitID][5]*(29/30) +  wantedOpacity*(1/30) --change gently
 			-- check if com is off the ground
 			if y-yg>10 then 
 				draw = false
@@ -203,49 +187,7 @@ function widget:GameFrame(n)
 			elseif not spIsSphereInView(x,y,z,blastRadius) then
 				draw = false
 			end
-			if draw and hideOnDistantEnemy then
-				local nearestEnemyUnitID = spGetUnitNearestEnemy(unitID,showOnEnemyDistance+fadeInDistance)
-				if nearestEnemyUnitID then
-					local ex,ey,ez = spGetUnitPosition(nearestEnemyUnitID)
-					local xDifference = x - ex
-					local yDifference = y - ey
-					local zDifference = z - ez
-					local distance = math.sqrt(xDifference*xDifference + yDifference*yDifference + zDifference*zDifference)
-					if distance < blastRadius + showOnEnemyDistance then
-						draw = true
-						opacityMultiplier = 1 - (distance - showOnEnemyDistance) / fadeInDistance
-						if opacityMultiplier > 1 then
-							opacityMultiplier = 1
-						elseif opacityMultiplier < 0 then
-							opacityMultiplier = 0
-						end
-						oldOpacityMultiplier = opacityMultiplier
-					end
-				else
-					opacityMultiplier = 0
-					oldOpacityMultiplier = opacityMultiplier
-					draw = false
-				end
-				
-				-- smooth out sudden changes of enemy unit distance
-				if comCenters[unitID] and comCenters[unitID][4] and comCenters[unitID][7] and opacityMultiplier ~= comCenters[unitID][7] and comCenters[unitID][6] and (osClock - comCenters[unitID][6]) < smoothoutTime then
-					draw = true
-					
-					local opacityDifference = comCenters[unitID][7] - opacityMultiplier
-					local opacityAddition = (1 - ((osClock - comCenters[unitID][6]) / smoothoutTime)) * opacityDifference
-					
-					opacityMultiplier = oldOpacityMultiplier + opacityAddition
-					if opacityMultiplier > 1 then
-						opacityMultiplier = 1
-					elseif opacityMultiplier < 0 then
-						opacityMultiplier = 0
-						draw = false
-					end
-					oldOpacityMultiplier = comCenters[unitID][7]	-- keep old
-					osClock = comCenters[unitID][6]					-- keep old
-				end
-			end
-			comCenters[unitID] = {x,y,z,draw,opacityMultiplier,osClock,oldOpacityMultiplier}
+			comCenters[unitID] = {x,y,z,draw,opacity}
 		else
 			--couldn't get position, check if its still a unit 
 			if not spValidUnitID(unitID) then
@@ -255,181 +197,25 @@ function widget:GameFrame(n)
 	end	
 end
 
+-- opacity control
+local darkOpacity = 0
+local lightOpacity = 0
+function SetOpacity(dark, light)
+    darkOpacity = dark
+    lightOpacity = light
+end
+
 -- draw circles
 function widget:DrawWorldPreUnit()
-	local camX, camY, camZ = spGetCameraPosition()
+    if spIsGUIHidden() then return end
 	glDepthTest(true)
-	glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 	for _,center in pairs(comCenters) do
 		if center[4] then
-			local xDifference = camX - center[1]
-			local yDifference = camY - center[2]
-			local zDifference = camZ - center[3]
-			local camDistance = math.sqrt(xDifference*xDifference + yDifference*yDifference + zDifference*zDifference)
-			
-			local lineWidthMinus = (camDistance/2000)
-			if lineWidthMinus > 2 then
-				lineWidthMinus = 2
-			end
-			local lineOpacityMultiplier = (1100/camDistance)*fadeMultiplier
-			if lineOpacityMultiplier > 1 then
-				lineOpacityMultiplier = 1
-			end
-			if center[5] then
-				lineOpacityMultiplier = lineOpacityMultiplier * center[5]
-			end
-			
-			if lineOpacityMultiplier > 0.05 then
-				if showTitles and camDistance < showTitleDistance then
-					local heightAddition = 5
-					
-					-- DGUN titles
-					local lineDistance	= 5
-					local fontSize		= 10
-					local radius		= dgunRange
-					local text			= "DGUN"
-					
-					glColor(1, 0.8, 0, .3*lineOpacityMultiplier)
-					glTranslate(center[1], 0, center[3])
-					
-					local mapHeight = spGetGroundHeight(center[1],center[3]-radius) + heightAddition
-					glTranslate(0,mapHeight,-radius)
-					glRotate(90,-1,0,0)
-					glText(text, 0, lineDistance, fontSize, "cn")
-					glRotate(90,1,0,0)
-					glTranslate(0,-mapHeight,radius)
-					
-					mapHeight = spGetGroundHeight(center[1],center[3]+radius) + heightAddition
-					glTranslate(0,mapHeight,radius)
-					glRotate(90,-1,0,0)
-					glRotate(180,0,0,1)
-					glText(text, 0, lineDistance, fontSize, "cn")
-					glRotate(180,0,0,-1)
-					glRotate(90,1,0,0)
-					glTranslate(0,-mapHeight,-radius)
-					
-					mapHeight = spGetGroundHeight(center[1]-radius,center[3]) + heightAddition
-					glTranslate(-radius,mapHeight,0)
-					glRotate(90,-1,0,0)
-					glRotate(90,0,0,1)
-					glText(text, 0, lineDistance, fontSize, "cn")
-					glRotate(90,0,0,-1)
-					glRotate(90,1,0,0)
-					glTranslate(radius,-mapHeight,0)
-					
-					mapHeight = spGetGroundHeight(center[1]+radius,center[3]) + heightAddition
-					glTranslate(radius,mapHeight,0)
-					glRotate(90,-1,0,0)
-					glRotate(270,0,0,1)
-					glText(text, 0, lineDistance, fontSize, "cn")
-					glRotate(270,0,0,-1)
-					glRotate(90,1,0,0)
-					glTranslate(-radius,-mapHeight,0)
-					
-					glTranslate(-center[1], 0, -center[3])
-					
-					
-					-- Com explosion range titles
-					lineDistance	= 5
-					fontSize		= 10
-					radius			= blastRadius
-					text			= "BLAST"
-					
-					glColor(1, 0, 0, .37*lineOpacityMultiplier)
-					glTranslate(center[1], 0, center[3])
-					
-					local mapHeight = spGetGroundHeight(center[1],center[3]-radius) + heightAddition
-					glTranslate(0,mapHeight,-radius)
-					glRotate(90,-1,0,0)
-					glText(text, 0, lineDistance, fontSize, "cn")
-					glRotate(90,1,0,0)
-					glTranslate(0,-mapHeight,radius)
-					
-					mapHeight = spGetGroundHeight(center[1],center[3]+radius) + heightAddition
-					glTranslate(0,mapHeight,radius)
-					glRotate(90,-1,0,0)
-					glRotate(180,0,0,1)
-					glText(text, 0, lineDistance, fontSize, "cn")
-					glRotate(180,0,0,-1)
-					glRotate(90,1,0,0)
-					glTranslate(0,-mapHeight,-radius)
-					
-					mapHeight = spGetGroundHeight(center[1]-radius,center[3]) + heightAddition
-					glTranslate(-radius,mapHeight,0)
-					glRotate(90,-1,0,0)
-					glRotate(90,0,0,1)
-					glText(text, 0, lineDistance, fontSize, "cn")
-					glRotate(90,0,0,-1)
-					glRotate(90,1,0,0)
-					glTranslate(radius,-mapHeight,0)
-					
-					mapHeight = spGetGroundHeight(center[1]+radius,center[3]) + heightAddition
-					glTranslate(radius,mapHeight,0)
-					glRotate(90,-1,0,0)
-					glRotate(270,0,0,1)
-					glText(text, 0, lineDistance, fontSize, "cn")
-					glRotate(270,0,0,-1)
-					glRotate(90,1,0,0)
-					glTranslate(-radius,-mapHeight,0)
-					
-					glTranslate(-center[1], 0, -center[3])
-				end
-				
-				local usedCircleDivs = math.floor(circleDivs*lineOpacityMultiplier)
-				if usedCircleDivs < 40 then
-					usedCircleDivs = 40
-				end
-				
-				-- draw lines
-				if showLineGlow then
-					glLineWidth(10)
-					glColor(1, 0.8, 0, .024*lineOpacityMultiplier)
-					glDrawGroundCircle(center[1], center[2], center[3], dgunRange, usedCircleDivs)
-					
-					glColor(1, 0, 0, .037*lineOpacityMultiplier)
-					glDrawGroundCircle(center[1], center[2], center[3], blastRadius, math.floor(usedCircleDivs*1.2))
-				end
-				glLineWidth(3-lineWidthMinus)
-				glColor(1, 0.8, 0, .24*lineOpacityMultiplier*opacityMultiplier)
-				glDrawGroundCircle(center[1], center[2], center[3], dgunRange, usedCircleDivs)
-				
-				glLineWidth(3.3-lineWidthMinus)
-				glColor(1, 0, 0, .4*lineOpacityMultiplier*opacityMultiplier)
-				glDrawGroundCircle(center[1], center[2], center[3], blastRadius, math.floor(usedCircleDivs*1.2))
-			end
+			glColor(1, 0.8, 0, min(center[5],lightOpacity))
+			glDrawGroundCircle(center[1], center[2], center[3], dgunRange, circleDivs)
+			glColor(1, 0, 0, min(center[5],darkOpacity))
+			glDrawGroundCircle(center[1], center[2], center[3], blastRadius, circleDivs)
 		end
 	end
-	glLineWidth(1)
 	glDepthTest(false)
-end
-
-function widget:GetConfigData(data)
-    savedTable = {}
-    savedTable.showLineGlow			= showLineGlow
-    savedTable.hideOnDistantEnemy	= hideOnDistantEnemy
-    return savedTable
-end
-
-function widget:SetConfigData(data)
-    if data.showLineGlow ~= nil 		then  showLineGlow			= data.showLineGlow end
-    if data.hideOnDistantEnemy ~= nil 	then  hideOnDistantEnemy	= data.hideOnDistantEnemy end
-end
-
-function widget:TextCommand(command)
-    if (string.find(command, "comranges_glow") == 1  and  string.len(command) == 14) then 
-		showLineGlow = not showLineGlow
-		if showLineGlow then
-			Spring.Echo("Comblast & Dgun Range:  Glow enabled")
-		else
-			Spring.Echo("Comblast & Dgun Range:  Glow disabled")
-		end
-	end
-    if (string.find(command, "comranges_nearbyenemy") == 1  and  string.len(command) == 21) then 
-		hideOnDistantEnemy = not hideOnDistantEnemy
-		if hideOnDistantEnemy then
-			Spring.Echo("Comblast & Dgun Range:  Hides ranges when enemy is nearby")
-		else
-			Spring.Echo("Comblast & Dgun Range:  Shows ranges regardless of enemy distance ")
-		end
-	end
 end

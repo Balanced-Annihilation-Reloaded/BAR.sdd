3b6a7db1915b55649642dddbb4cef83285c9aa6d
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-08-09 20:39:16 +0000

    chilify energy conversion & auto res sharing

--- branches/BAR/luaui/widgets/bgu_resbars.lua
+++ branches/BAR/luaui/widgets/bgu_resbars.lua
@@ -6,7 +6,6 @@ function widget:GetInfo()
 		author  = 'Funkencool',
 		date    = '2013',
 		license = 'GNU GPL v2',
-		layer		= 0,
 		enabled = true,
 	}
 end
@@ -24,12 +23,15 @@ local image = {
 	metal  = 'luaui/images/resourcebars/Ibeam.png',
 	energy = 'luaui/images/resourcebars/lightning.png',
 	}
+local conversionPic  = "LuaUI/Images/EMconversion.png"
+
 local meter        = {}
 local incomeLabel  = {}
 local expenseLabel = {}
 local netLabel     = {}
 local shareLevel   = {}
 local myTeamID = spGetMyTeamID()
+local settings = {}
 -- Colors
 local green        = {0.2, 1.0, 0.2, 1.0}
 local red          = {1.0, 0.2, 0.2, 1.0}
@@ -60,6 +62,19 @@ end
 -------------------------------------------
 -- Main
 -------------------------------------------
+local function ToggleConversionPanel()
+    if conversionPanel.hidden then
+        conversionPanel:Show()
+        conversionButton2:Show()
+        window0:Hide()
+    else
+        conversionPanel:Hide()    
+        conversionButton2:Hide()
+        window0:Show()
+    end
+    return true
+end
+
 local function initWindow()
 	local screen0 = Chili.Screen0
 	
@@ -70,7 +85,7 @@ local function initWindow()
 		width     = 450, 
 		height    = 60, 
 		minHeight = 20, 
-		padding   = {0,0,0,0}
+		padding   = {0,0,0,0},
 	}
 
 end
@@ -107,7 +122,7 @@ local function makeBar(res, barX, barY)
 	
 	netLabel[res] = Chili.Label:New{
 		caption = '',
-		x       = 5,
+		x       = 7,
 		bottom  = 7,
 		parent  = control,
 	}
@@ -140,6 +155,123 @@ local function makeBar(res, barX, barY)
 	
 end
 
+local function makeConversionPanel()
+    conversionButton = Chili.Button:New{ --for when window0 is shown
+        parent = window0,
+        x = 0,
+        bottom = 0,
+        height = 35,
+        width = 35,
+        onclick = {ToggleConversionPanel},
+        caption = "",
+        padding = {7,7,7,7},
+        children = {Chili.Image:New{width='100%',height='100%',file=conversionPic}},
+    }
+    
+    conversionPanel = Chili.Window:New{
+        parent = Chili.Screen0,
+        height = 60,
+        width = 450-35,
+        right = 0,
+        y = 0,
+        padding = {10,10,10,10}
+    }    
+    
+    conversionButton2 = Chili.Button:New{ --for when conversionPanel is shown
+        parent = Chili.Screen0,
+        right = 415, --450-35
+        y = 60-35,
+        height = 35,
+        width = 35,
+        onclick = {ToggleConversionPanel},
+        caption = "",
+        padding = {7,7,7,7},
+        children = {Chili.Image:New{width='100%',height='100%',file=conversionPic}},
+    }
+
+    conversionText = Chili.TextBox:New{
+        parent = conversionPanel,
+        x = 0,
+        y = 0,
+        height = 18,
+        width = 197,
+        text = " E to M conversion above this %",
+        font = {
+            size = 12,
+        }    
+    }
+    
+    local function SetConversion(obj, value)
+        local alterLevelFormat = string.char(137) .. '%i'
+        Spring.SendLuaRulesMsg(string.format(alterLevelFormat, value))
+        Spring.Echo("Conversion of energy to metal will only occur when energy is above " .. value .. "% full")
+    end
+    
+    local function SetEShare (obj, value)
+        Spring.SetShareLevel('energy', value)
+        Spring.Echo("Energy will be shared to allies when over " .. value .. "% full")
+    end
+
+    local function SetMShare (obj, value)
+        Spring.SetShareLevel('metal', value)        
+        Spring.Echo("Metal will be shared to allies when over " .. value .. "% full")
+    end
+    
+    conversionSlider = Chili.Trackbar:New{
+        parent = conversionPanel,
+        height = 25,
+        width = 190,
+        x = 0,
+        y = 18,
+        min = 0,
+        max = 100,
+        step = 5,
+        value = 90,
+        onchange = {SetConversion},
+    }
+    
+    shareText = Chili.TextBox:New{
+        parent = conversionPanel,
+        x = 197,
+        y = 0,
+        height = 18,
+        width = 197,
+        text = " share (E,M) to allies above this %",
+        font = {
+            size = 12,
+        }    
+    }
+
+    EshareSlider = Chili.Trackbar:New{
+        parent = conversionPanel,
+        height = 25,
+        width = 95,
+        x = 197,
+        y = 18,
+        min = 0,
+        max = 100,
+        step = 5,
+        value = 95,
+        onchange = {SetEShare},
+    }
+
+    MshareSlider = Chili.Trackbar:New{
+        parent = conversionPanel,
+        height = 25,
+        width = 95,
+        x = 197+95+5,
+        y = 18,
+        min = 0,
+        max = 100,
+        step = 5,
+        value = 95,
+        onchange = {SetMShare},
+    }
+
+    conversionPanel:Hide()
+    conversionButton2:Hide()
+end
+
 -- Updates 
 local function setBar(res)
 	local currentLevel, storage, pull, income, expense, share = spGetTeamResources(myTeamID, res)
@@ -170,14 +302,19 @@ end
 -- Callins
 -------------------------------------------
 function widget:GameFrame(n)
-	if n%2 == 0 then
-		setBar('metal')
-		setBar('energy')
-	end
+    setBar('metal')
+    setBar('energy')
 end
 
 function widget:CommandsChanged()
 	myTeamID = spGetMyTeamID()
+end
+
+function SetValues()
+    if not settings[1] then return end
+    EshareSlider:SetValue(settings[1])
+    MshareSlider:SetValue(settings[2])
+    conversionSlider:SetValue(settings[3])
 end
 
 function widget:Initialize()
@@ -186,11 +323,19 @@ function widget:Initialize()
 	initWindow()
 	makeBar('metal',0,0)
 	makeBar('energy',30,30)
+    makeConversionPanel()
     if Spring.GetGameFrame()>0 then
         SetBarColors()
     else
         meter['metal']:SetColor(0.0,0.6,0.9,.8)
         meter['energy']:SetColor(0.0,0.6,0.9,.8)        
+    end
+    SetValues()
+end
+
+function widget:KeyPress()
+    if window0.hidden then
+        ToggleConversionPanel()
     end
 end
 
@@ -200,4 +345,15 @@ end
 
 function widget:Shutdown()
 	Spring.SendCommands('resbar 1')
+end
+
+function widget:GetConfigData()
+    settings[1] = EshareSlider.value
+    settings[2] = MshareSlider.value
+    settings[3] = conversionSlider.value    
+    return settings
+end
+
+function widget:SetConfigData(data)
+    settings = data
 end

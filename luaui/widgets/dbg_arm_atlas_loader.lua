1bb1485641478a15172baa23aaa92f96aab561e9
Bluestone <3885930+silentwings@users.noreply.github.com>
2014-08-09 11:35:54 +0000

    attempt at fixing #636

---
+++ luaui/widgets/dbg_arm_atlas_loader.lua
@@ -0,0 +1,33 @@
+
+function widget:GetInfo()
+	return {
+		name	= 'Arm Atlas Loader',
+		desc	= 'Forces the arm atlas to be loaded',
+		author	= 'Bluestone',
+		date	= '',
+		license	= 'GNU GPL v3',
+		layer	= 0, -- must go after initial queue, or depthtest is wrong
+		enabled	= true,
+	}
+end
+
+-- this is a bit of a hack!
+
+local n = 0
+local ARMCOM = UnitDefNames["armcom"].id
+
+function widget:DrawWorld()
+    n  = n + 1
+
+    if n==1 then
+        gl.PushMatrix()
+            gl.Translate(-1000, -1000, -1000)
+            gl.UnitShape(ARMCOM, 0)
+        gl.PopMatrix()
+    end
+    
+    if n>1 then
+        widgetHandler:RemoveWidget()
+    end
+
+end

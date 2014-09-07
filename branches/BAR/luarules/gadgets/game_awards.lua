c701881a5b5eb9536a76cc56a8c3718d17c99f85
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2014-09-07 14:17:38 +0000

    fix lua error

--- branches/BAR/luarules/gadgets/game_awards.lua
+++ branches/BAR/luarules/gadgets/game_awards.lua
@@ -136,12 +136,13 @@ function gadget:UnitCreated(unitID, unitDefID, teamID, builderID)
     --Spring.Echo(teamID, teamInfo[teamID].unitsCost)
 end
 
-function gadget:UnitTaken(unitID, unitDefID, unitTeam, newTeam)
+function gadget:UnitTaken(unitID, unitDefID, teamID, newTeam)
 	if not newTeam then return end 
     if newTeam==gaiaTeamID then return end
 	if not present[newTeam] then return end
 	if not unitDefID then return end --should never happen
-
+    if not teamID then return end
+    
 	local ud = UnitDefs[unitDefID]
 	local cost = ud.energyCost + 60 * ud.metalCost
 

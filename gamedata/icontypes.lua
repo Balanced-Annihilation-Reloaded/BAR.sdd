b207da6b6a593765b2c9b933ec4aa7909b654d92
Bluestone <3885930+silentwings@users.noreply.github.com>
2014-09-10 06:43:14 +0000

    remove dead code

--- gamedata/icontypes.lua
+++ gamedata/icontypes.lua
@@ -3,49 +3,12 @@ local icontypes = {
     size=1.3,
     radiusadjust=1,
   },
-  corthovr = {
-    size=0.7,
-    radiusadjust=1,
-  },
-  armthovr = {
-    size=0.9,
-    radiusadjust=1,
-  },
-  
-  armcommander = {
-    bitmap="icons/armcom.tga",
-    size=2,
-  },
-  
-  corcommander = {
-    bitmap="icons/corcom.tga",
-    size=2,
-    radiusadjust=1,
-  },
-  
-  krogoth = {
-    bitmap="icons/krogoth.tga",
-    size=3,
-  },
-  
-  air = {
-    bitmap="icons/triangle.tga",
-    size=1.2,
-  },
-  
-  sea = {
-    bitmap="icons/semi-circle.tga",
-    size=1.5,
-  },
-  
-  building = {
-    bitmap="icons/square.tga",
-    size=0.8,
-  },  
   blank = {
     bitmap="icons/blank.tga",
     size=1,
   },
+  -- icons are set in the gadget unit_customicons.lua
+  
   
   chicken = 
 	{

5e8a32703d01ce7f27de997f7b8de2cef872be3d
Bluestone <3885930+silentwings@users.noreply.github.com>
2015-01-19 10:59:37 +0000

    fix #837

--- luaui/widgets/api_unit_texture_preloader.lua
+++ luaui/widgets/api_unit_texture_preloader.lua
@@ -19,7 +19,7 @@ end
 local files = {
 	--"unittextures/Arm_color.dds", --preloading these (they are used by S30) seems to have no effect
 	--"unittextures/Arm_other.dds",
-	"unittextures/Core_normals.dds", -- this does reduce the stall when first unit using this is constructed, but also it should only preload if luaunitshader are enabled? (else its a waste of time and gpu ram)
+	"unittextures/Core_normal.dds", -- this does reduce the stall when first unit using this is constructed, but also it should only preload if luaunitshader are enabled? (else its a waste of time and gpu ram)
 	"unittextures/Arm_normals.dds",
 }
 

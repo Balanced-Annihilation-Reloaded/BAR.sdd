2aad938a992133801f6e5deb04ac8fa54d61774b
Floris <Floris@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-01-21 19:00:09 +0000

    Now scales with camera distance + outer color is white with dark colored names + performance improvement: doesnt render when com isnt in view

--- branches/BAR/luaui/widgets/gui_com_nametags.lua
+++ branches/BAR/luaui/widgets/gui_com_nametags.lua
@@ -2,8 +2,8 @@ function widget:GetInfo()
   return {
     name      = "Commander Name Tags",
     desc      = "Displays a name tag above each commander.",
-    author    = "Bluestone",
-    date      = "July 2014",
+    author    = "Bluestone, Floris",
+    date      = "January 2015",
     license   = "GNU GPL, v2 or later",
     layer     = -10,
     enabled   = false,  --  loaded by default?
@@ -11,23 +11,40 @@ function widget:GetInfo()
 end
 
 --------------------------------------------------------------------------------
+-- config
+--------------------------------------------------------------------------------
 
-local GetUnitTeam         = Spring.GetUnitTeam
-local GetTeamInfo         = Spring.GetTeamInfo
-local GetPlayerInfo       = Spring.GetPlayerInfo
-local GetTeamColor        = Spring.GetTeamColor
-local GetVisibleUnits     = Spring.GetVisibleUnits
-local GetUnitDefID        = Spring.GetUnitDefID
-local GetAllUnits         = Spring.GetAllUnits
+local heightOffset			= 28
+local fontSize				= 13
+local scaleFontAmount		= 130
 
-local glDepthTest         = gl.DepthTest
-local glAlphaTest         = gl.AlphaTest
-local glColor             = gl.Color
-local glText              = gl.Text
-local glTranslate         = gl.Translate
-local glBillboard         = gl.Billboard
-local glDrawFuncAtUnit    = gl.DrawFuncAtUnit
-local GL_GREATER          = GL.GREATER
+local font = gl.LoadFont("Fonts/FreeSansBold.otf",50, 8, 8)
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local GetUnitTeam        		= Spring.GetUnitTeam
+local GetTeamInfo        		= Spring.GetTeamInfo
+local GetPlayerInfo      		= Spring.GetPlayerInfo
+local GetTeamColor       		= Spring.GetTeamColor
+local GetVisibleUnits    		= Spring.GetVisibleUnits
+local GetUnitDefID       		= Spring.GetUnitDefID
+local GetAllUnits        		= Spring.GetAllUnits
+local IsUnitInView	 	 		= Spring.IsUnitInView
+local GetCameraPosition  		= Spring.GetCameraPosition
+local GetUnitPosition    		= Spring.GetUnitPosition
+
+local glDepthTest        		= gl.DepthTest
+local glAlphaTest        		= gl.AlphaTest
+local glColor            		= gl.Color
+local glText             		= gl.Text
+local glTranslate        		= gl.Translate
+local glBillboard        		= gl.Billboard
+local glDrawFuncAtUnit   		= gl.DrawFuncAtUnit
+local GL_GREATER     	 		= GL.GREATER
+local GL_SRC_ALPHA				= GL.SRC_ALPHA	
+local GL_ONE_MINUS_SRC_ALPHA	= GL.ONE_MINUS_SRC_ALPHA
+local glBlending          		= gl.Blending
 
 --------------------------------------------------------------------------------
 
@@ -35,30 +52,73 @@ local comms = {}
 
 --------------------------------------------------------------------------------
 
-local function DrawName(name,colour,height)
-  glTranslate(0, height, 0 )
-  glBillboard()  
-  glColor(colour)
-  if (colour[1]+colour[2]+colour[3]>0.5) then
-    glText(name, 0, 0, 13, "co")
+--gets the name, color, and height of the commander
+local function GetCommAttributes(unitID, unitDefID)
+  local team = GetUnitTeam(unitID)
+  if team == nil then
+    return nil
   end
-  glText(name, 0, 0, 13, "c")
-  glColor(1,1,1,1)
+  local _, player = GetTeamInfo(team)
+  local name,_,_,_,_,_,_,country,rank = GetPlayerInfo(player)
+  local r, g, b, a = GetTeamColor(team)
+  local height = UnitDefs[unitDefID].height + heightOffset
+  return {name or 'Commander', {r, g, b, a}, height, country, rank}
 end
+
+
+local function DrawName(unitID, attributes)
+  
+  local iconHeight = (12.5+usedFontSize/1.6)
+  
+  glTranslate(0, attributes[3], 0 )
+  glBillboard()
+   
+  font:Begin()
+  font:SetTextColor(attributes[2])
+  
+  -- not acurate (enough)   but...   font:SetAutoOutlineColor(true)   doesnt seem to work
+  if (attributes[2][1] + attributes[2][2]*1.35 + attributes[2][3]*0.5) < 0.75 then
+    font:SetOutlineColor({1,1,1,1})
+  else
+    font:SetOutlineColor({0,0,0,1})
+  end
+  font:Print(attributes[1], -0.3, 0, usedFontSize, "con")
+  
+  font:End()
+end
+
 
 local vsx, vsy = Spring.GetViewGeometry()
 function widget:ViewResize()
   vsx,vsy = Spring.GetViewGeometry()
 end
 
+
 function widget:DrawWorld()
+  --if Spring.IsGUIHidden() then return end
+
   glDepthTest(true)
   glAlphaTest(GL_GREATER, 0)
-
-  for unitID, _ in pairs(comms) do
-    glDrawFuncAtUnit(unitID, false, DrawName, comms[unitID].name, comms[unitID].colour, comms[unitID].height)
+  glBlending(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+   
+  local camX, camY, camZ = GetCameraPosition()
+  
+  for unitID, attributes in pairs(comms) do
+    
+    -- calc opacity
+	if IsUnitInView(unitID) then
+		local x,y,z = GetUnitPosition(unitID)
+		local xDifference = camX - x
+		local yDifference = camY - y
+		local zDifference = camZ - z
+		camDistance = math.sqrt(xDifference*xDifference + yDifference*yDifference + zDifference*zDifference) 
+		
+	    usedFontSize = (fontSize*0.5) + (camDistance/scaleFontAmount)
+	    
+		glDrawFuncAtUnit(unitID, false, DrawName, unitID, attributes)
+	end
   end
-
+  
   glAlphaTest(false)
   glColor(1,1,1,1)
   glDepthTest(false)
@@ -90,7 +150,7 @@ function CheckAllComs()
   for _, unitID in ipairs(units) do
     local unitDefID = GetUnitDefID(unitID)
     if (unitDefID and UnitDefs[unitDefID].customParams.iscommander) then
-      comms[unitID] = CreateName(unitID, unitDefID)
+      comms[unitID] = GetCommAttributes(unitID, unitDefID)
     end
   end
 end

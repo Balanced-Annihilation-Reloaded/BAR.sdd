085e3df3a7887b847340f26271495216502901aa
Bluestone <3885930+silentwings@users.noreply.github.com>
2015-08-01 09:21:50 +0000

    add explosions_post.lua

--- gamedata/alldefs_post.lua
+++ gamedata/alldefs_post.lua
@@ -2,20 +2,24 @@
 -- DOCUMENTATION
 -------------------------
 
--- BA contains weapondefs in its unitdef files
--- Standalone weapondefs are only loaded by Spring after unitdefs are loaded
--- So, if we want to do post processing and include all the unit+weapon defs, and have the ability to bake these changes into files, we must do it after both have been loaded
--- That means, ALL UNIT AND WEAPON DEF POST PROCESSING IS DONE HERE
+-- This file contains BAR _Post fucntions, and the ability to bake any changes they make into files
+-- These won't touch defs in dependencies/mutators, and may react badly to mutators using _post
 
 -- What happens:
 -- unitdefs_post.lua calls the _Post functions for unitDefs and any weaponDefs that are contained in the unitdef files
 -- unitdefs_post.lua writes the corresponding unitDefs to customparams (if wanted)
 -- weapondefs_post.lua fetches any weapondefs from the unitdefs, 
 -- weapondefs_post.lua fetches the standlaone weapondefs, calls the _post functions for them, writes them to customparams (if wanted)
--- strictly speaking, alldefs.lua is a misnomer since this file does not handle armordefs, featuredefs or movedefs
+-- the other defs_post.lua files just call the _Post function here, and then write (if wanted)
 
--- Switch for when we want to save defs into customparams as strings (so as a widget can then write them to file)
--- The widget to do so can be found in 'etc/Lua/bake unitdefs_post'
+-- WHY WEAPON/UNITDEFS ARE SPECIAL:
+-- BA contains weapondefs in its unitdef files
+-- Standalone weapondefs are only loaded by Spring after unitdefs are loaded
+-- So, if we want to do post processing and include all the unit+weapon defs, and have the ability to bake these changes into files, we must do it after both have been loaded (which means, here)
+
+
+-- Master switch for when we want to save defs into customparams as strings (so as a widget can then write them to file)
+-- The widget to do so can be found in 'etc/Lua/bake _post'
 SaveDefsToCustomParams = false
 
 
@@ -31,6 +35,25 @@ end
 -- process weapondef
 function WeaponDef_Post(name, wDef)
 
+end
+
+-- process effects
+function ExplosionDef_Post(name, eDef)
+    --[[
+    -- WIP on #645
+    Spring.Echo(name)
+    for k,v in pairs(eDef) do
+        Spring.Echo(" ", k, v, type(k), type(v))
+        if type(v)=="table" then
+            for k1,v1 in pairs(v) do
+                Spring.Echo("  ", k1,v1)
+            end
+        end
+    end
+    if eDef.usedefaultexplosions=="1" then
+    
+    end
+    ]]
 end
 
 

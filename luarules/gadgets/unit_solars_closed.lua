8ca469c629a83ccae8bb49d0e04c8b369296074c
Bluestone <3885930+silentwings@users.noreply.github.com>
2015-08-02 09:27:49 +0000

    clean way for solars not to produce e when closed

---
+++ luarules/gadgets/unit_solars_closed.lua
@@ -0,0 +1,38 @@
+function gadget:GetInfo()
+  return {
+    name      = "Closed Solars",
+    desc      = "Closed solar collectors don't produce energy",
+    author    = "Bluestone",
+    date      = "",
+    license   = "Hats",
+    layer     = 0,
+    enabled   = true  --  loaded by default?
+  }
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+if (not gadgetHandler:IsSyncedCode()) then
+  return
+end
+
+local noClosedE = {
+    [UnitDefNames.corsolar.id] = true,
+    [UnitDefNames.armsolar.id] = true,
+}
+
+function gadget:UnitCommand(unitID, unitDefID, teamID, cmdID, cmdParams, cmdOptions)
+    if noClosedE[unitDefID] and cmdID==CMD.ONOFF then
+        local on = (cmdOptions[1]==1) --invert because the command hasn't yet taken effect
+        local _,_,eMake,_ = Spring.GetUnitResources(unitID)
+        if on then 
+            Spring.SetUnitResourcing(unitID, "ume", UnitDefs[unitDefID].energyMake - eMake)
+        else
+            Spring.SetUnitResourcing(unitID, "ume", -eMake)
+        end
+    end
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------

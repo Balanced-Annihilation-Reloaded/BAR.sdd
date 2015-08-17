eddc39df91cb64186f793886a932664ca1448dcb
Bluestone <3885930+silentwings@users.noreply.github.com>
2015-08-17 08:00:45 +0000

    remove dead code

--- luaui/widgets/bgu_awards.lua
+++ luaui/widgets/bgu_awards.lua
@@ -11,61 +11,11 @@ function widget:GetInfo()
 end
 
 
---[[                HOW AWARDS WORK
-    
- >> Normal Awards
-
-These ones get a 1st, 2nd and 3rd place which are included in that order in the LuaMsg.
-
-ecoKillAward is for killing the most enemy econ production. fightKillAward is for killing 
-the most enemy fighting stuff. effKillAward is for the best damage 
-done <-> resources used ratio. The gadget has a mechanism that tries to account for 
-taking/sharing.
-
- >> Special Awards
-
-These awards are won by a single team; there is no first/second/third.
-
-The CowAward ("Memorial WarCow") is given to a team that wins (first place in) all three normal 
-awards. It's quite hard to win a WarCow.
-
-There are three other special awards: ecoAward  (most eco produced), dmgRecAward (most damage taken) 
-and sleepAward (not doing damage for longest). The sleepAward counts time not doing damage even 
-after a teams death; it is often won by someone who did something stupid and died very early.
-
- >> Pictures
-
-The three KillAwards and the CowAward have pictures associated to them which you can find 
-in /luaui/images/awards. The other awards are in some sense less important and don't have pictures.
- 
- >> TeamIDs, playerIDs, playerNames
-
-For all awards, they are only awarded when particular criteria are met so there is always the 
-chance that the award is not given to anyone. The awards gadget assigns awards to teams, so the numbers you 
-receive are the teamIDs of team(s) who won that award and (if appropriate) the corresponding score.
-
-Of course, what then appears on screen are the names of players who won. The correspondance is: 
-for each teamID the gadget looks for a playerID of a player who was not a spectator at GameStart 
-(recall specs are put in team 0, which also contains some genuine players). The first such playerID 
-found is used to name the player who gets the award. If more than one such playerID is found 
-then .. " (coop)" is added to the players name.    
-
-Please put this documentation somewhere sensible when you are done with the example widget! 
-    
-]]
-
 ------------
 -- Vars
 
 local Chili, container, stackPanel
 local playerListByTeam = {}
-
-local qIndex = 1
-local quotes = {
-    "No one, you all lose.",
-    "Again, No one. BORING!",
-    "Did you actually play the game?",
-}
 
 ------------
 -- Auxillary Functions
@@ -139,12 +89,9 @@ local function createAward(award)
         parent   = container,
         x        = 0,
         width    = 550,
-        height   = award.height or 100,
+        height   = 100,
         children = {
-            Chili.Label:New{x=0 ,y=0,caption=award.title,font={size=20}},
-            Chili.Label:New{x=20,y=20,caption=award.first or ''},
-            Chili.Label:New{x=20,y=35,caption=award.second or ''},
-            Chili.Label:New{x=20,y=50,caption=award.third or ''}
+            Chili.Label:New{x=0 ,y=0,caption="test award"
         },
     }
 end
@@ -155,7 +102,7 @@ end
 function widget:Initialize()
     if not WG.Chili then return end
     
-    widgetHandler:RegisterGlobal('ReceiveAwards', ReceiveAwards)
+    widgetHandler:RegisterGlobal('AwardAward', AwardAward)
     
     -- init Chili
     Chili = WG.Chili
@@ -176,85 +123,22 @@ function widget:Initialize()
 end
 
 function widget:ShutDown()
-    widgetHandler:DeregisterGlobal('ReceiveAwards')
+    widgetHandler:DeregisterGlobal('AwardAward')
 end
 
 function widget:GameOver()
     Spring.SendCommands('endgraph 0')    
 end
 
--- this function will be magically called just after gameover
--- you need at least two teams in a game to test awards (otherwise the game does not ever end)
--- simplest way is to make a short replay with $VERSION and use that for testing
-function ReceiveAwards( ecoKillAward, ecoKillAwardSec, ecoKillAwardThi, ecoKillScore, ecoKillScoreSec, ecoKillScoreThi, 
-                        fightKillAward, fightKillAwardSec, fightKillAwardThi, fightKillScore, fightKillScoreSec, fightKillScoreThi, 
-                        effKillAward, effKillAwardSec, effKillAwardThi, effKillScore, effKillScoreSec, effKillScoreThi, 
-                        ecoAward, ecoScore, 
-                        dmgRecAward, dmgRecScore, 
-                        sleepAward, sleepScore,
-                        cowAward)
+function AwardAward(name, action, first, first_score, second, second_score, third, third_score)
 
     -- Create the chili element containing the awards
     createContainer()
     --
     
-    
-    
-    
-    -- WIP!!!
-    if true then return end
+    if true then return end --WIP    
 
-
-    
-    if ecoKillAward > 0 then
-    
-        local ecoKill = {}
-        ecoKill.title = "Most economy destroyed"
-        ecoKill.first = "This award goes to "..FindPlayerName(ecoKillAward)..", with a score of "..ecoKillScore
-        
-        if ecoKillAwardSec > 0 then
-            ecoKill.second = "In a close second, "..FindPlayerName(ecoKillAwardSec).." had a score of "..ecoKillScoreSec
-        end
-        
-        if ecoKillAwardThi > 0 then
-            ecoKill.third = "Behind both is "..FindPlayerName(ecoKillAwardThi).." with a score of "..ecoKillScoreThi
-        end
-        
-        createAward(ecoKill)
-    end
-    --
-    if fightKillAward > 0 then
-    
-        local fightKill = {}
-        fightKill.title = "Most enemy combantants destroyed"
-        fightKill.first = "This award goes to "..FindPlayerName(fightKillAward)..", with a score of "..fightKillScore
-        
-        if fightKillAwardSec > 0 then
-            fightKill.second = "In a close second, "..FindPlayerName(fightKillAwardSec).." had a score of "..fightKillScoreSec
-        end
-        
-        if fightKillAwardThi > 0 then
-            fightKill.third = "Behind both is "..FindPlayerName(fightKillAwardThi).." with a score of "..fightKillScoreThi
-        end
-        
-        createAward(fightKill)
-    end
-    --
-    if effKillAward > 0 then
-    
-        local effKill = {}
-        effKill.title = "Most efficient use of units"
-        effKill.first = "This award goes to "..FindPlayerName(effKillAward)..", with a score of "..effKillScore
-        
-        if fightKillAwardSec > 0 then
-            effKill.second = "In a close second, "..FindPlayerName(effKillAwardSec).." had a score of "..effKillScoreSec
-        end
-        
-        if fightKillAwardThi > 0 then
-            effKill.third = "Behind both is "..FindPlayerName(effKillAwardThi).." with a score of "..effKillScoreThi
-        end
-        
-        createAward(effKill)
-    end
+    local award = {}
+    createAward(award)
     
 end

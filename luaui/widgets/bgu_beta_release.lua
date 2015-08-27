27c4708d765f8e134a7f690bd89ccd46b4da1f7d
Bluestone <3885930+silentwings@users.noreply.github.com>
2015-08-27 16:10:25 +0000

    add chickens to beta release menu

--- luaui/widgets/bgu_beta_release.lua
+++ luaui/widgets/bgu_beta_release.lua
@@ -32,16 +32,77 @@ end
 local options = {}
 local info = {}
 local script = {}
---[[options.chickens = {
-    ["CHICKEN_DIFFICULTY"] = {},
-    ["CHICKEN_QUEEN_DIFFICULTY"] = {},
-    ["MAPNAME"] = {},
-}]]
-options["AI Skirmish"] = {
-    ["MAPNAME"] = {
+options["Chickens"] = {
+    [1] = {
+        name = "MAP_NAME",
         humanName = "Map",
-        [1] = "Tundra", --TODO: choose maps
-        [2] = "Talus",
+        options = {
+            [1] = "Chicken Roast",
+            [2] = "Chicken Nuggets",
+        },
+        replace = {
+            [1] = "Chicken_Roast_v1",
+            [2] = "Chicken_Nuggets_v4"
+        
+        }
+    },
+    [2] = {
+        name = "CHICKEN_DIFFICULTY",
+        humanName = "Difficulty",
+        options = {
+            [1] = "Very Easy",
+            [2] = "Easy",
+            [3] = "Medium",
+            [4] = "Hard",
+            [5] = "Very Hard",
+            [6] = "Epic",
+            [7] = "Survival",
+        },
+        replace = {
+            [1] = "Chicken: Very Easy",
+            [2] = "Chicken: Easy",
+            [3] = "Chicken: Medium",
+            [4] = "Chicken: Hard",
+            [5] = "Chicken: Very Hard",
+            [6] = "Chicken: Epic",
+            [7] = "Chicken: Survival",               
+        }
+    },
+    [3] = {
+        name = "CHICKEN_QUEEN_DIFFICULTY",
+        humanName = "Queen",
+        options = {
+            [1] = "Very Easy",
+            [2] = "Easy",
+            [3] = "Medium",
+            [4] = "Hard",
+            [5] = "Very Hard",
+            [6] = "Epic",
+            [7] = "Ascending",        
+        },
+        replace = {
+            [1] = "ve_chickenq",
+            [2] = "e_chickenq",
+            [3] = "n_chickenq",
+            [4] = "h_chickenq",
+            [5] = "vh_chickenq",
+            [6] = "ve_chickenq",
+            [7] = "epic_chickenq",
+            [8] = "asc"
+        },   
+    },
+}
+info["Chickens"] = "Fight against hoards of oncoming chickens!\n\nStart positions are randomly chosen."
+script["Chickens"] = ReadScript("chickens.txt")
+
+options["AI Skirmish"] = {
+    [1] = {
+        name = "MAP_NAME",
+        humanName = "Map",
+        options = {
+            [1] = "Tundra", --TODO: choose maps
+            [2] = "Talus",
+        }
     },
 }
 info["AI Skirmish"] = "Fight against an AI!\n\nStart positions are randomly chosen."
@@ -73,28 +134,40 @@ local function Load (self)
     
     textInfoBox:SetText(info[gameMode])
     
-    for name,choices in pairs(options) do
+    local i = 0
+    local w_pc = 100/3
+    for _,config in ipairs(options) do
         local thisOption = Chili.LayoutPanel:New{
             parent = optionsBox,
-            name = name,
-            width = tostring(100/math.max(2,#options)) .. '%',
+            name = config.name,
+            width = tostring(w_pc) .. '%',
+            x = tostring(w_pc*i) .. '%',
             height = '100%',
             orientation = "vertical", 
             resizeItems = false,
+            padding = {0,0,0,0},
+            --itemPadding = {0,0,0,0},
+            --itemMargin = {0,0,0,0},
+            
         }
         Chili.Label:New{
             parent = thisOption,
-            caption = choices.humanName,
+            caption = config.humanName,
             width = '100%',
             height = 20,
+            y = 5,
             
         }
         Chili.ComboBox:New{
             parent = thisOption,
             name = "comboBox",
-            items = choices,   
+            items = config.options,   
+            replace = config.replace or {},
             width = '100%',
+            y = 25,
         }
+        
+        i = i + 1
     end
 
 end
@@ -107,7 +180,8 @@ function Start()
     for _,child in ipairs(optionsBox.children) do
         local name = child.name
         local comboBox = child:GetChildByName("comboBox")
-        local choice = comboBox.items[comboBox.selected]
+        local choice = comboBox.replace[comboBox.selected] or comboBox.items[comboBox.selected] 
+        Spring.Echo(name, comboBox.replace[comboBox.selected], comboBox.items[comboBox.selected] )
         startScript = string.gsub(startScript, name, choice)    
     end
     
@@ -129,18 +203,21 @@ local function AddToMenu()
     
     optionsBox = Chili.Panel:New{
         parent  = container,
-        x       = '50%',
+        x       = '30%',
         y       = '0%',
-        width   = '50%',
+        width   = '70%',
         height  = '20%',    
+            padding = {0,0,0,0},
+            itemPadding = {0,0,0,0},
+            itemMargin = {0,0,0,0},
     }
 
     textInfoBox = Chili.TextBox:New{width='100%',text=initialText, padding = {8,8,8,8}}    
     textInfoScrollPanel = Chili.ScrollPanel:New{
         parent      = container,
-        x           = '50%',
+        x           = '30%',
         y           = '20%',
-        width       = '50%',
+        width       = '70%',
         height      = '50%',
         resizeItems = false,
         autosize    = true,
@@ -153,9 +230,9 @@ local function AddToMenu()
     textWarningBox = Chili.TextBox:New{width='100%',text="", padding = {8,8,8,8}}    
     textWarningScrollPanel = Chili.ScrollPanel:New{
         parent      = container,
-        x           = '50%',
+        x           = '30%',
         y           = '70%',
-        width       = '50%',
+        width       = '70%',
         height      = '20%',
         resizeItems = false,
         autosize    = true,
@@ -181,20 +258,20 @@ local function AddToMenu()
         parent = container,
         x = 0,
         y = 0,
-        width = '50%',
+        width = '30%',
         height = '100%',
         itemMargin = {2,2,2,2},
         orientation = "vertical",
         resizeItems = true,
         children = {
             Chili.Line:New{width = '50%', height = 2},
-            Chili.Button:New{height = 70, width = '80%', caption = "AI Skirmish", OnClick = {Load},},
+            Chili.Button:New{height = 70, width = '90%', caption = "AI Skirmish", OnClick = {Load},},
             Chili.Line:New{width = '50%', height = 2},
-            Chili.Button:New{height = 70, width = '80%', caption = "Chickens (Tower Defence)", OnClick = {Load},},
+            Chili.Button:New{height = 70, width = '90%', caption = "Chickens", OnClick = {Load},},
             Chili.Line:New{width = '50%', height = 2},
-            Chili.Button:New{height = 70, width = '80%', caption = "Mission 1: Glacier", OnClick = {Load},},
-            Chili.Button:New{height = 70, width = '80%', caption = "Mission 2: ?", OnClick = {Load},},
-            Chili.Button:New{height = 70, width = '80%', caption = "Mission 3: ?", OnClick = {Load},},
+            Chili.Button:New{height = 70, width = '90%', caption = "Mission 1: Glacier", OnClick = {Load},},
+            Chili.Button:New{height = 70, width = '90%', caption = "Mission 2: ?", OnClick = {Load},},
+            Chili.Button:New{height = 70, width = '90%', caption = "Mission 3: ?", OnClick = {Load},},
             Chili.Line:New{width = '50%', height = 2},
         }    
     }

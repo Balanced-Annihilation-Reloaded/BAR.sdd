bf409cd4ed101645f46575a0767cdf74195b1d40
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2015-09-02 22:03:33 +0000

    fix #926

---
+++ branches/BAR/luaui/widgets/gui_transfered.lua
@@ -0,0 +1,48 @@
+function widget:GetInfo()
+  return {
+    name      = "Transfer Announce", 
+    desc      = "Tells you when unit/resource transfers take place",
+    author    = "Bluestone", 
+    date      = "",
+    license   = "GPL v2 or later",
+    layer     = 0, 
+    enabled   = true
+  }
+end
+
+local white = "\255\255\255\255"
+
+function InlineColour(R,G,B)
+    local r,g,b
+    if type(R) == 'table' then
+        r,g,b = math.max(1,R[1]*255),math.max(1,R[2]*255),math.max(1,R[3]*255)
+    else
+        r,g,b = math.max(1,R*255),math.max(1,G*255),math.max(1,B*255)
+    end
+    return string.char(255,r,g,b)
+end
+
+function GetTeamPlayerName(teamID)
+    local r,g,b = Spring.GetTeamColor(teamID)
+    local colourString = InlineColour(r,g,b)
+    
+    local skirmishAIID, name, hostPlayerID, shortName, version = Spring.GetAIInfo(teamID)
+    local playerList = Spring.GetPlayerList(teamID)
+    local name = shortName or select(1,Spring.GetPlayerInfo(playerList[1])) or ""
+    
+    if #playerList<=1 then return colourString .. name
+    else return colourString .. name .. " (coop)"
+    end    
+end
+
+function ObjectTransfered(teamID, newTeamID, object, amount)
+    Spring.Echo(GetTeamPlayerName(teamID) .. white .. " sent " .. amount .. " " .. object .. " to " .. GetTeamPlayerName(newTeamID))
+end
+
+function widget:Initialize()
+    widgetHandler:RegisterGlobal("ObjectTransfered", ObjectTransfered)
+end
+
+function widget:ShutDown()
+    widgetHandler:DeregisterGlobal("ObjectTransfered")
+end

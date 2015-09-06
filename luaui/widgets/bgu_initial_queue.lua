d6647fa1152e21caf668ba73bc4caca4c3afdabf
Bluestone <3885930+silentwings@users.noreply.github.com>
2015-09-06 17:57:16 +0000

    fix initial queue leaving stuff behind in WG when forcibly removed

--- luaui/widgets/bgu_initial_queue.lua
+++ luaui/widgets/bgu_initial_queue.lua
@@ -331,8 +331,8 @@ function widget:Initialize()
     if (Game.startPosType == 1) or            -- Don't run if start positions are random
        (Spring.GetGameFrame() > 0) or        -- Don't run if game has already started
        (amNewbie) or                        -- Don't run if i'm a newbie
-       (Spring.GetSpectatingState()) or
-       (WG.isMission) then    -- Don't run if we are a spec
+       (Spring.GetSpectatingState()) or   -- Don't run if we are a spec
+       (WG.isMission) then    
         widgetHandler:RemoveWidget(self)
         return
     end
@@ -366,7 +366,7 @@ function widget:Initialize()
 end
 
 function widget:Shutdown()
-
+    WG.InitialQueue = nil
 end
 
 ------------------------------------------------------------

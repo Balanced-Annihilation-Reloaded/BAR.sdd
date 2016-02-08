1ec173e3ec4c94203a86047429a76a78e8f5dbb0
abma <abma@users.noreply.github.com>
2016-02-08 22:33:14 +0000

    fix typos

--- units/corfmmm.lua
+++ units/corfmmm.lua
@@ -53,7 +53,7 @@ return {
 				height = 20,
 				hitdensity = 100,
 				metal = 242,
-				object = "CORUWMMM_DEAD.s3o",
+				object = "corfmmm_dead.s3o",
 				reclaimable = true,
 				seqnamereclamate = "TREE1RECLAMATE",
 				world = "All Worlds",

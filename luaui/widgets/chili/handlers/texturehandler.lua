bf730a9c92cf7c26237d3e567940cd9389d03282
Bluestone <3885930+silentwings@users.noreply.github.com>
2016-02-10 18:29:50 +0000

    update chili

--- luaui/widgets/chili/handlers/texturehandler.lua
+++ luaui/widgets/chili/handlers/texturehandler.lua
@@ -32,6 +32,8 @@ local placeholderFilename = theme.skin.icons.imageplaceholder
 local placeholderDL = gl.CreateList(gl.Texture,CHILI_DIRNAME .. "skins/default/empty.png")
 --local placeholderDL = gl.CreateList(gl.Texture,placeholderFilename)
 
+local isEngineTexture = { [string.byte("!")] = true, [string.byte("%")] = true, [string.byte("#")] = true, [string.byte("$")] = true, [string.byte("^")] = true }
+
 local function AddRequest(filename,obj)
   local req = requested
   if (req[filename]) then
@@ -71,13 +73,17 @@ function TextureHandler.LoadTexture(arg1,arg2,arg3)
      filename = arg1
      obj = arg2
   end
-
+  
   local tex = loaded[filename]
-  if (not tex) then
-    AddRequest(filename,obj)
-    glActiveTexture(activeTexID,glCallList,placeholderDL)
-  else
+  if (tex) then
     glActiveTexture(activeTexID,glCallList,tex.dl)
+  else
+    AddRequest(filename,obj)
+    if isEngineTexture[filename:byte(1)] then
+      gl.Texture(activeTexID, filename)
+    else
+      glActiveTexture(activeTexID,glCallList,placeholderDL)
+    end
   end
 end
 
@@ -114,13 +120,13 @@ function TextureHandler.Update()
 
 		if (usedTime<0) then usedTime = 0 end
 	end
-    
+
 	local broken = {}
 	local timerStart = spGetTimer()
 	local finished = false
 	while (usedTime < timeLimit)and(not finished) do
 		local filename,objs = next(requested)
-        
+
 		if (filename) then
 			gl.Texture(filename)
 			gl.Texture(false)

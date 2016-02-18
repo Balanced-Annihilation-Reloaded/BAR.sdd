8af57544e64942367d222564736271814ae84ccc
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2016-02-18 00:37:29 +0000

    remove dead code

--- branches/BAR/luarules/gadgets/unit_dynamic_collision_volume.lua
+++ branches/BAR/luarules/gadgets/unit_dynamic_collision_volume.lua
@@ -52,11 +52,7 @@ if (gadgetHandler:IsSyncedCode()) then
                 local featureModel = FeatureDefs[Spring.GetFeatureDefID(featID)].modelname:lower()
                 if featureModel:len() > 4 then
                     local featureModelTrim
-                    if Game.version > "91.0" then
-                        featureModelTrim = featureModel:sub(1,-5) -- featureModel:match("/.*%."):sub(2,-2)
-                    else
-                        featureModelTrim = featureModel:match("/.*%."):sub(2,-2)
-                    end
+                    featureModelTrim = featureModel:sub(1,-5) -- featureModel:match("/.*%."):sub(2,-2)
                     if mapFeatures[featureModelTrim] then
                         local p = mapFeatures[featureModelTrim]
                         spSetFeatureCollisionData(featID, p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
@@ -65,11 +61,7 @@ if (gadgetHandler:IsSyncedCode()) then
                         local xs, ys, zs, xo, yo, zo, vtype, htype, axis, _ = spGetFeatureCollisionData(featID)
                         Spring.Echo(featureModel, xs, ys, zs, xo, yo, zo, vtype, htype, axis)
                         if (vtype>=3 and xs==ys and ys==zs) then
-                            if Game.version > "91.0" then
-                                spSetFeatureCollisionData(featID, xs, ys*0.75, zs,  xo, yo-ys*0.09, zo,  1, htype, 1)
-                            else
-                                spSetFeatureCollisionData(featID, xs, ys*0.75, zs,  xo, yo-ys*0.09, zo,  vtype, htype, axis)
-                            end
+                            spSetFeatureCollisionData(featID, xs, ys*0.75, zs,  xo, yo-ys*0.09, zo,  1, htype, 1)
                         end
                     end
                 end

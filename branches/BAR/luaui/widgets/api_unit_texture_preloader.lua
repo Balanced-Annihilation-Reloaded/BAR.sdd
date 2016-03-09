7f90420464382080c37ba3119b53ce4c79e6e615
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2016-03-09 13:41:40 +0000

    BAR: Preload everything

--- branches/BAR/luaui/widgets/api_unit_texture_preloader.lua
+++ branches/BAR/luaui/widgets/api_unit_texture_preloader.lua
@@ -29,22 +29,28 @@ local files = {
 function widget:DrawGenesis()
     --
     -- workaround for https://springrts.com/mantis/view.php?id=5136
+	local f_cnt = 0
+	local u_cnt = 0
     for udid, ud in pairs(UnitDefs) do
+		Spring.PreloadUnitDefModel(udid)
+		u_cnt = u_cnt + 1
         local temp = ud.model.midx
     end
     for fdid, fd in pairs(FeatureDefs) do
+		Spring.PreloadFeatureDefModel(fdid)
+		f_cnt = f_cnt + 1
         local temp = fd.model.midx
     end
-    --
     
     
 	if Spring.GetGameFrame() < 1 then 
 		for i,file in ipairs(files) do
 			gl.Texture(7,file)
 			gl.Texture(7,false)
-			Spring.Echo('Preloaded',file)
+			-- Spring.Echo('Preloaded',file)
 		end
 	end
+    Spring.Echo('Preloaded',u_cnt,'units',f_cnt,'features and ',#files,' textures.')
     widgetHandler:RemoveWidget()
 end
 

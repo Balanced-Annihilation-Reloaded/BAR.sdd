ac2a621945110d7bb032a8829fdbf554c84ee1b7
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2016-03-10 19:25:53 +0000

    fix #1057

--- branches/BAR/luarules/gadgets/chicken_teamcolor.lua
+++ branches/BAR/luarules/gadgets/chicken_teamcolor.lua
@@ -1,31 +1,24 @@
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
 
-function widget:GetInfo()
+function gadget:GetInfo()
   return {
     name      = "Chicken Team Colour",
     desc      = "Changes the Chicken team colour as the queen gets angrier",
     author    = "TheFatController",
     date      = "Aug 07, 2011",
     license   = "MIT",
-    layer     = -9, 
+    layer     = 0, 
     enabled   = true  --  loaded by default?
   }
 end
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
-if (not Spring.GetGameRulesParam("difficulty")) then
+local enabled = tonumber(Spring.GetModOptions().mo_chickengame) or 0
+if (enabled == 0) then 
   return false
 end
 
 local chickenTeamID = Spring.GetGameRulesParam("chickenTeamID")
 
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
-function widget:GameFrame(n)
+function gadget:GameFrame(n)
   if (n == 1) then
     chickenTeamID = Spring.GetGameRulesParam("chickenTeamID")
   end
@@ -35,6 +28,7 @@ function widget:GameFrame(n)
     queenTime = (Spring.GetGameRulesParam("queenTime") or 0)
     queenAnger = 1 - math.ceil((((Spring.GetGameSeconds()-gracePeriod+queenAnger)/(queenTime-gracePeriod))*100) -0.5) / 100
     Spring.SetTeamColor(chickenTeamID, 1,queenAnger,queenAnger)
+    Spring.Echo(1,queenAnger,queenAnger)
   end
 end
 

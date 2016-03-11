4099909527d211f9a484610e61e7a65915fd0d1d
Bluestone <3885930+silentwings@users.noreply.github.com>
2016-03-11 10:17:35 +0000

    move cmd_holdfire_immediately to unsynced gadget

--- luarules/gadgets/cmd_holdfire_immediately.lua
+++ luarules/gadgets/cmd_holdfire_immediately.lua
@@ -1,8 +1,8 @@
 
-function widget:GetInfo()
+function gadget:GetInfo()
     return {
-        name      = "Holdfire Instantly",
-        desc      = "Holdfire commands take effect immediately",
+        name      = "Hold fire Instantly",
+        desc      = "Hold fire commands take effect immediately",
         author    = "Niobium",
         date      = "3 April 2010",
         license   = "GNU GPL, v2 or later",
@@ -11,13 +11,19 @@ function widget:GetInfo()
     }
 end
 
+if gadgetHandler:IsSyncedCode() then
+    return false
+end
+
+
 local CMD_FIRE_STATE = CMD.FIRE_STATE
 local CMD_INSERT = CMD.INSERT
 local CMD_STOP = CMD.STOP
 local spGiveOrder = Spring.GiveOrder
 
-function widget:CommandNotify(cmdID, cmdParams, cmdOpts)
+function gadget:CommandNotify(cmdID, cmdParams, cmdOptions)
     if (cmdID == CMD_FIRE_STATE) and (cmdParams[1] == 0) then
+        Spring.Echo("hold")
         spGiveOrder(CMD_INSERT, {0, CMD_STOP, 0}, {"alt"})
     end
 end

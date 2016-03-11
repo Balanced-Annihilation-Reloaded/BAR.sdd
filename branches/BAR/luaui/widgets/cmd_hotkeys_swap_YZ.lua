8d0d178fd51647a1a020e0f9f87eab8e767cddec
Bluestone <Bluestone@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2016-03-11 10:54:30 +0000

    one more widget cat

--- branches/BAR/luaui/widgets/cmd_hotkeys_swap_YZ.lua
+++ branches/BAR/luaui/widgets/cmd_hotkeys_swap_YZ.lua
@@ -1,7 +1,7 @@
 function widget:GetInfo()
     return {
-        name = "Hotkeys -- swap Y and Z",
-        desc = "Swaps Y and Z keys in hotkeys widget (useful for AZERTY keyboards)" ,
+        name = "Hotkey Y-Z Swap",
+        desc = "Swaps Y and Z keys in BARs hotkeys (useful for AZERTY keyboards)" ,
         author = "Beherith, Bluestone",
         date = "",
         license = "GNU LGPL, v2.1 or later",
@@ -15,7 +15,7 @@ function widget:Initialize()
         WG.swapYZbinds = true
         WG.SetYZState()
     else
-        Spring.Echo("Hotkeys widget not found, cannot swap YZ")
+        Spring.Echo("BAR hotkeys widget not found, cannot swap YZ")
         widgetHandler:RemoveWidget(self)
     end
 end
@@ -25,6 +25,6 @@ function widget:Shutdown()
     if WG.SetYZState then
         WG.SetYZState()
     else
-        Spring.Echo("BA Hotkeys widget not found, cannot swap YZ")
+        Spring.Echo("BAR hotkeys widget not found, cannot swap YZ")
     end
 end

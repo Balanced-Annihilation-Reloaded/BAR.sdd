26b01be624ca9d3241a85b65ef26326bbf922967
Bluestone <3885930+silentwings@users.noreply.github.com>
2016-03-22 15:25:21 +0000

    probably fix #1084

--- luaui/widgets/unit_cloaked_states.lua
+++ luaui/widgets/unit_cloaked_states.lua
@@ -17,20 +17,18 @@ local CMD_FIRE_STATE = CMD.FIRE_STATE
 local CMD_MOVE_STATE = CMD.MOVE_STATE
 
 local myPlayerID = Spring.GetMyPlayerID()
+local myTeamID = Spring.GetMyTeamID()
 
 local cloakedUnits = {} --cloakedUnits[unitID={savedFireState=,savedMoveState=}
 local onlySelectedCloakedUnits = false
 
-function UnloadIfSpec()
-    local _, _, spec, _, _, _, _, _ = Spring.GetPlayerInfo(myPlayerID)
-    if ( spec == true ) then
-        widgetHandler:RemoveWidget()
-        return 
-    end
-    return 
+function widget:PlayerChanged()
+    myTeamID = Spring.GetMyTeamID()
 end
 
 function widget:UnitCloaked(unitID, unitDefID, unitTeam)
+    if unitTeam~=myTeamID then return end
+
     local state = Spring.GetUnitStates(unitID)
     cloakedUnits[unitID] = {savedFireState=state.firestate, savedMoveState=state.movestate}    
     Spring.GiveOrderToUnit(unitID, CMD_MOVE_STATE, {0}, 0)
@@ -38,10 +36,21 @@ function widget:UnitCloaked(unitID, unitDefID, unitTeam)
 end
 
 function widget:UnitDecloaked(unitID, unitDefID, unitTeam)
+    if unitTeam~=myTeamID then return end
+
     if not cloakedUnits[unitID] or not Spring.ValidUnitID(unitID) then return end
     Spring.GiveOrderToUnit(unitID, CMD_MOVE_STATE, {cloakedUnits[unitID].savedMoveState}, 0) 
     Spring.GiveOrderToUnit(unitID, CMD_FIRE_STATE, {cloakedUnits[unitID].savedFireState}, 0) 
     cloakedUnits[unitID] = nil
+end
+
+function widget:UnitTaken(unitID, unitDefID, oldTeam, newTeam)
+    if newTeam==myTeamID then
+        local state = Spring.GetUnitStates(unitID)
+        if state and state.cloak then
+            widget:UnitCloaked(unitID, unitDefID, myTeamID)        
+        end
+    end
 end
 
 function widget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam)
@@ -49,8 +58,6 @@ function widget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerD
 end
 
 function widget:Initialize()
-    UnloadIfSpec()
-    
     local units = Spring.GetAllUnits()
     for _,unitID in ipairs(units) do
         local state = Spring.GetUnitStates(unitID)
@@ -72,7 +79,7 @@ function widget:CommandsChanged()
     for unitID,_ in pairs(cloakedUnits) do
         foundCloakedUnit = true
         selUnits = selUnits or Spring.GetSelectedUnits() -- its cheaper this way
-        if not cloakedUnits[unitID] then
+        if not selUnits[unitID] then
             onlySelectedCloakedUnits = false
             break
         end

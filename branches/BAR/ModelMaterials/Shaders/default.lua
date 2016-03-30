d80f3100f2e8e36f3715d3656b95eebea19cb470
beherith <beherith@fec744b0-2096-477f-bb2d-5cae9d2fa2dc>
2016-03-30 14:24:43 +0000

    BAR: fix specular/reflection in CUS

--- branches/BAR/ModelMaterials/Shaders/default.lua
+++ branches/BAR/ModelMaterials/Shaders/default.lua
@@ -160,14 +160,14 @@ vertex = [[
 		vec4 extraColor = texture2D(textureS3o2, gl_TexCoord[0].st);
 		vec3 reflectDir = reflect(cameraDir, normal);
 		
-		#if (deferred_mode == 0)
+		//#if (deferred_mode == 0)
 			vec3 specular   = textureCube(specularTex, reflectDir).rgb * extraColor.g * SPECULARMULT;
 			vec3 reflection = textureCube(reflectTex,  reflectDir).rgb;
-		#endif
-		#if (deferred_mode == 1) 
-			vec3 specular   = vec3(1.0,1.0,1.0) * extraColor.g * SPECULARMULT;
-			vec3 reflection = vec3(0.0,0.0,0.0);
-		#endif
+		// #endif
+		// #if (deferred_mode == 1) 
+			// vec3 specular   = vec3(0.1,0.1,0.1) * extraColor.g * SPECULARMULT;
+			// vec3 reflection = vec3(0.0,0.0,0.0);
+		// #endif
 		float shadow = GetShadowCoeff(gl_TexCoord[1] + vec4(0.0, 0.0, -0.00005, 0.0));
 		light     = mix(sunAmbient, light, shadow);
 		specular *= shadow;
